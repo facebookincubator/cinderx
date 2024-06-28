e74e22b0670ba1d980d635219e9067192e8dbab3
Jacob Bower <jbower@meta.com>
2024-06-28 14:14:09 -0700

    Get non-opt 3.12 python builds working for CinderX development
    Reviewed By: itamaro
    
    Differential Revision: D58483313
    
    fbshipit-source-id: 9cc7a91da392153ca15f93b59ded8f4f27449a45

--- TestScripts/test_build_modes.sh
+++ TestScripts/test_build_modes.sh
@@ -3,21 +3,69 @@
 set -xe
 set -o pipefail
 
-cd "$(dirname $(readlink -f $0))"/../
+cd "$(dirname "$(readlink -f "$0")")"/../
 
-# # Only debug builds have sys.gettotalrefcount()
-buck2 run @fbcode//mode/dbg fbcode//cinderx:python3.10 -- -c 'import sys; sys.gettotalrefcount()'
-buck2 run @fbcode//mode/dev fbcode//cinderx:python3.10 -- -c 'import sys; sys.gettotalrefcount()'
-! buck2 run @fbcode//mode/opt fbcode//cinderx:python3.10 -- -c 'import sys; sys.gettotalrefcount()'
+function fail() {
+    set +x
+    echo -en '\n\033[1;31m'
+    echo "$@"
+    echo -en '\033[1;0m\n'
+    exit 1
+}
 
-# # Only ASAN builds should have the ASAN API avilable through ctypes
-! buck2 run @fbcode//mode/dbg fbcode//cinderx:python3.10 -- -c 'import ctypes; ctypes.pythonapi.__asan_default_options()'
-buck2 run @fbcode//mode/dev fbcode//cinderx:python3.10 -- -c 'import ctypes; ctypes.pythonapi.__asan_default_options()'
-! buck2 run @fbcode//mode/opt fbcode//cinderx:python3.10 -- -c 'import ctypes; ctypes.pythonapi.__asan_default_options()'
+function run_buck2() {
+    local mode=$1
+    shift
+    buck2 run -c cinderx.use_3_12="$use_3_12" @fbcode//mode/"$mode" "$@"
+}
 
-# We embed build flags in our objects
-strings $(buck2 run @//mode/dev fbcode//cinderx:dist-path)/lib/libpython3.10.so | ( ! grep -- "-O3 -fno-omit-frame-pointer" > /dev/null )
-strings $(buck2 run @//mode/dbg fbcode//cinderx:dist-path)/lib/libpython3.10.so | ( ! grep -- "-O3 -fno-omit-frame-pointer" > /dev/null )
-strings $(buck2 run @//mode/opt fbcode//cinderx:dist-path)/lib/libpython3.10.so | grep -- "-O3 -fno-omit-frame-pointer" > /dev/null
+function run_python_code() {
+    local mode=$1
+    local code=$2
+    local expect_except=$3
+    if run_buck2 "$mode" fbcode//cinderx:python -- -c "$code" ; then
+        if [[ "$expect_except" == "except" ]] ; then
+            fail "Expected '$1' run of '$2' to fail"
+        fi
+    else
+        if [[ "$expect_except" == "except" ]] ; then
+            return
+        fi
+        fail "Expected '$1' run of '$2' to pass"
+    fi
+}
 
-echo All tests passed!
+function check_opt_flags() {
+    local mode=$1
+    local should_opt=$2
+    local libpython=$(echo $(run_buck2 "$mode" fbcode//cinderx:dist-path)/lib/libpython3.*.so)
+    if strings "$libpython" | grep -- "-O3 -fno-omit-frame-pointer" >/dev/null ; then
+        if [[ "$should_opt" == "yes" ]] ; then
+            return
+        fi
+        fail "Expected to not find opt flags with build mode $mode"
+    else
+        if [[ "$should_opt" == "yes" ]] ; then
+            fail "Expected to find opt flags with build mode $mode"
+        fi
+    fi
+}
+
+for use_3_12 in true false ; do
+    # # Only debug builds have sys.gettotalrefcount()
+    run_python_code dbg 'import sys; sys.gettotalrefcount()'
+    run_python_code dev 'import sys; sys.gettotalrefcount()'
+    run_python_code opt 'import sys; sys.gettotalrefcount()' except
+
+    # # Only ASAN builds should have the ASAN API avilable through ctypes
+    run_python_code dbg 'import ctypes; ctypes.pythonapi.__asan_default_options()' except
+    run_python_code dev 'import ctypes; ctypes.pythonapi.__asan_default_options()'
+    run_python_code opt 'import ctypes; ctypes.pythonapi.__asan_default_options()' except
+
+    # We embed build flags in our objects
+    check_opt_flags dev no
+    check_opt_flags dbg no
+    check_opt_flags opt yes
+done
+
+echo -e '\n\033[1;32m**All tests passed!**\033[1;0m\n'
