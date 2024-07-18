4f412274680dcc23eb6ce47998d22c1e80f41f0b
Sergio Ly <sergio40@meta.com>
2024-07-17 17:10:35 -0700

    Back out "Port over gh-91048 for awaiter support in Meta Python 3.12"
    Summary:
    Original commit changeset: ca0d2ee0ffca
    
    Original Phabricator Diff: D57072594
    
    Reverting due to segfaults. [Details](https://fb.workplace.com/groups/cinderusers/permalink/3478626758950016/)
    
    Reviewed By: aleivag
    
    Differential Revision: D59866335
    
    fbshipit-source-id: cae113f4d729e73dc71f02405e3264e1170b5988

--- Upgrade/upgrade_stubs.h
+++ Upgrade/upgrade_stubs.h
@@ -235,6 +235,8 @@ PyObject* CiCoro_New_NoFrame(PyThreadState *tstate, PyCodeObject *code);
 PyObject* CiAsyncGen_New_NoFrame(PyCodeObject *code);
 PyObject* CiGen_New_NoFrame(PyCodeObject *code);
 
+void _PyAwaitable_SetAwaiter(PyObject *receiver, PyObject *awaiter);
+typedef void (*setawaiterfunc)(PyObject *receiver, PyObject *awaiter);
 typedef struct {
     PyAsyncMethods ame_async_methods;
     sendfunc ame_send;
