a13fde3cf6b520e21402f0ac40d966f937d76538
Dino Viehland <dinoviehland@meta.com>
2024-07-25 09:46:47 -0700

    Move vtable implementation targets
    Summary:
    Take 2 of D59864306. The previous version of this diff hit some issues with non x64 builds of Cinder and got reverted. The issue is that the v-table thunks (as defined in `vtable_defs.h`) for x64 are just defined as `void thunk_name(void);` and this is fine, these never get called from C code and the prologue is in assembly. There's a backup definition for non-x64 platforms which has argument types though and merely calls the vector call implementation, therefore it needs the args. This aligns the definitions so they all have argument types.
    
    We have a lot of different v-table entries that get plugged into the v-table when we have things other than simple functions. These handle all of the various delicacies of dispatching to the real function.
    
    This pulls all of those entry points out into a new library that can live on its own. There's one exception: the lazy init entry is what gets populated into the v-table before it's been invoked for the first time. That continues to live in classloader.c.
    
    Reviewed By: alexmalyshev
    
    Differential Revision: D60180678
    
    fbshipit-source-id: ca5a1014431607cf9072667d3742869c99eec195

---
+++ StaticPython/vtable_defs.h
@@ -0,0 +1,125 @@
+/* Copyright (c) Meta Platforms, Inc. and affiliates. */
+
+#pragma once
+
+#include <Python.h>
+#include "cinderx/StaticPython/thunks.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// Defines a helper thunk with the same layout as our JIT generated static
+// entry points.  This has the static entry point at the start, and then 11
+// instructions in we have the vectorcall entry point.  We install the static
+// entry points into the v-table and can easily switch to the vector call
+// form when we're invoking from the interpreter or somewhere that can't use
+// the native calling convention.
+#if defined(_M_X64) || defined(_M_AMD64) || defined(__x86_64__)
+#define VTABLE_THUNK(name, _arg1_type)                                            \
+  __attribute__((naked)) PyObject* name##_dont_bolt(_arg1_type* state,              \
+                                                    PyObject** args,                \
+                                                    size_t nargsf) {                \
+    __asm__(                                                                        \
+                /* static_entry: */                                                 \
+                /* we explicitly encode the jmp forward to static_entry_impl so */  \
+                /* that we always get the 2 byte version.  The 0xEB is the jump, */ \
+                /* the 14 is the length to jump, which is based upon the size of */ \
+                /* jmp to the vectorcall entrypoint */                              \
+                ".byte 0xEB\n"                                                      \
+                ".byte 14\n"                                                        \
+                "nop\n"                                                             \
+                "nop\n"                                                             \
+                "nop\n"                                                             \
+                "nop\n"                                                             \
+                "nop\n"                                                             \
+                "nop\n"                                                             \
+                "nop\n"                                                             \
+                "nop\n"                                                             \
+                "nop\n"                                                             \
+                                                                                    \
+                /* vector_entry: */                                                 \
+                "jmp " #name "_vectorcall\n"                                        \
+                                                                                    \
+                /* static_entry_impl: */                                            \
+                "push %rbp\n"                                                       \
+                "mov %rsp, %rbp\n"                                                  \
+                "push %rsp\n"                                                       \
+                /* We want to push the arguments passed natively onto the stack */  \
+                /* so that we can recover them in hydrate_args.  So we push them */ \
+                /* onto the stack and then move the address of them into rsi */     \
+                /* which will make them available as the 2nd argument.  Note we */  \
+                /* don't need to push rdi as it's the state argument which we're */ \
+                /* passing in anyway */                                             \
+                "push %r9\n"                                                        \
+                "push %r8\n"                                                        \
+                "push %rcx\n"                                                       \
+                "push %rdx\n"                                                       \
+                "push %rsi\n"                                                       \
+                "mov %rsp, %rsi\n"                                                  \
+                "call " #name "_native\n"                                           \
+                /* We don't know if we're returning a floating point value or not */\
+                /* so we assume we are, and always populate the xmm registers */    \
+                /* even if we don't need to */                                      \
+                "movq %rax, %xmm0\n"                                                \
+                "movq %rdx, %xmm1\n"                                                \
+                "leave\n"                                                           \
+                "ret\n"                                                             \
+        ); \
+  }
+#else
+#define VTABLE_THUNK(name, arg1_type)                     \
+  PyObject* name##_dont_bolt(                             \
+      arg1_type* state, PyObject** args, size_t nargsf) { \
+    return name##_vectorcall(state, args, nargsf);        \
+  }
+#endif
+
+
+typedef struct _PyClassLoader_StaticCallReturn {
+  void* rax;
+  void* rdx;
+} _PyClassLoader_StaticCallReturn;
+
+vectorcallfunc _PyClassLoader_GetStaticFunctionEntry(PyFunctionObject* func);
+
+static const _PyClassLoader_StaticCallReturn StaticError = {0, 0};
+
+// Converts native arguments that were pushed onto the stack into an
+// array of PyObject** args that can be used to call into the interpreter.
+int _PyClassLoader_HydrateArgs(
+    PyCodeObject* code,
+    Py_ssize_t arg_count,
+    void** args,
+    PyObject** call_args,
+    PyObject** free_args);
+
+// Frees the arguments which were hydrated with _PyClassLoader_HydrateArgs
+void _PyClassLoader_FreeHydratedArgs(PyObject** free_args, Py_ssize_t arg_count);
+
+// Gets the number of arguments that this callable will be called with
+// when dispatched via a v-table entry.
+Py_ssize_t _PyClassLoader_GetExpectedArgCount(PyObject** callable);
+
+PyObject* _PyVTable_coroutine_vectorcall(
+    _PyClassLoader_TypeCheckState* state,
+    PyObject* const* args,
+    size_t nargsf);
+
+PyObject* _PyVTable_func_lazyinit_dont_bolt(PyObject* state, PyObject** args, size_t nargsf);
+PyObject* _PyVTable_thunk_ret_primitive_not_jitted_dont_bolt(PyObject* state, PyObject** args, size_t nargsf);
+PyObject* _PyVTable_coroutine_classmethod_dont_bolt(_PyClassLoader_TypeCheckState* state, PyObject** args, size_t nargsf);
+PyObject* _PyVTable_coroutine_property_dont_bolt(_PyClassLoader_TypeCheckState* state, PyObject** args, size_t nargsf);
+PyObject* _PyVTable_coroutine_dont_bolt(_PyClassLoader_TypeCheckState* state, PyObject** args, size_t nargsf);
+PyObject* _PyVTable_nonfunc_property_dont_bolt(_PyClassLoader_TypeCheckState* state, PyObject** args, size_t nargsf);
+PyObject* _PyVTable_func_overridable_dont_bolt(_PyClassLoader_TypeCheckState* state, PyObject** args, size_t nargsf);
+PyObject* _PyVTable_classmethod_overridable_dont_bolt(_PyClassLoader_TypeCheckState* state, PyObject** args, size_t nargsf);
+PyObject* _PyVTable_nonfunc_dont_bolt(_PyClassLoader_TypeCheckState* state, PyObject** args, size_t nargsf);
+PyObject* _PyVTable_staticmethod_dont_bolt(PyObject* state, PyObject** args, size_t nargsf);
+PyObject* _PyVTable_classmethod_dont_bolt(PyObject* state, PyObject** args, size_t nargsf);
+PyObject* _PyVTable_thunk_vectorcall_only_dont_bolt(PyObject* state, PyObject** args, size_t nargsf);
+PyObject* _PyVTable_descr_dont_bolt(PyObject* state, PyObject** args, size_t nargsf);
+PyObject* _PyVTable_func_missing_dont_bolt(PyObject* state, PyObject** args, size_t nargsf);
+#ifdef __cplusplus
+}
+#endif
