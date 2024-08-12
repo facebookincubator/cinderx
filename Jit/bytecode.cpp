d7ebe40dc347ea2c8feb6d439033522fedf80316
Alex Malyshev <alexanderm@meta.com>
2024-08-12 07:39:12 -0700

    Mark more instructions as jumps and terminators
    Summary: Haven't updated these sets since adding the new opcodes.
    
    Reviewed By: mpage
    
    Differential Revision: D60607937
    
    fbshipit-source-id: 3acbc267bbb2a7dd4c0314e2dcc28efb87fb5f79

--- Jit/bytecode.cpp
+++ Jit/bytecode.cpp
@@ -12,30 +12,46 @@ namespace jit {
 const std::unordered_set<int> kBranchOpcodes = {
     FOR_ITER,
     JUMP_ABSOLUTE,
+    JUMP_BACKWARD,
+    JUMP_BACKWARD_NO_INTERRUPT,
     JUMP_FORWARD,
     JUMP_IF_FALSE_OR_POP,
     JUMP_IF_NONZERO_OR_POP,
+    JUMP_IF_NOT_EXC_MATCH,
     JUMP_IF_TRUE_OR_POP,
     JUMP_IF_ZERO_OR_POP,
-    JUMP_IF_NOT_EXC_MATCH,
     POP_JUMP_IF_FALSE,
+    POP_JUMP_IF_NONE,
+    POP_JUMP_IF_NONZERO,
+    POP_JUMP_IF_NOT_NONE,
     POP_JUMP_IF_TRUE,
     POP_JUMP_IF_ZERO,
-    POP_JUMP_IF_NONZERO,
 };
 
 const std::unordered_set<int> kRelBranchOpcodes = {
     FOR_ITER,
+    JUMP_BACKWARD,
+    JUMP_BACKWARD_NO_INTERRUPT,
     JUMP_FORWARD,
+    POP_JUMP_IF_NONE,
+    POP_JUMP_IF_NOT_NONE,
+    SEND,
     SETUP_FINALLY,
+
+#if PY_VERSION_HEX >= 0x030B0000
+    // These instructions switched from absolute to relative in 3.11.
+    POP_JUMP_IF_FALSE,
+    POP_JUMP_IF_TRUE,
+#endif
 };
 
 // we always consider branches block terminators; no need to duplicate them here
 const std::unordered_set<int> kBlockTerminatorOpcodes = {
-    RETURN_VALUE,
-    RETURN_PRIMITIVE,
     RAISE_VARARGS,
     RERAISE,
+    RETURN_CONST,
+    RETURN_PRIMITIVE,
+    RETURN_VALUE,
 };
 
 } // namespace jit
