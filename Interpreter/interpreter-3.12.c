62efb24e8e8047baecbb751f76ec1084e992e01a
Martin DeMello <martindemello@meta.com>
2024-09-04 14:16:00 -0700

    Start a 3.12 fork of the interpreter
    Summary:
    * Copy python 3.12's `ceval.c` and some supporting headers into an `cinderx/Interpreter/Includes` subdirectory. These shoud ideally not need to be edited.
    * Copy `bytecode.c` into `cinderx/Interpreter` and add a `regen-cases.sh` script to regenerate the generated cases manually (this is a stopgap measure until we add a buck target to do this)
    * Add an `interpreter-3.12.c` that simply includes `ceval.c` for now
    * Add an `:Interpreter3.12` target to build the new interpreter lib
    
    Reviewed By: jbower-fb
    
    Differential Revision: D62047729
    
    fbshipit-source-id: c3f485d2ed498bda0558807be2920c70aad1cff1

---
+++ Interpreter/interpreter-3.12.c
@@ -0,0 +1,6 @@
+// Copyright (c) Meta Platforms, Inc. and affiliates.
+
+#include "cinderx/Interpreter/opcode.h"
+
+#define CINDERX_INTERPRETER
+#include "Includes/ceval.c"
---
+++ Interpreter/regen-cases.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
+
+# Copied from cpython Makefile
+# Point srcdir to the internal copy of cpython 3.12 so we can use the cases_generator
+srcdir='../../../third-party/python/3.12'
+
+PYTHONPATH=$srcdir/Tools/cases_generator \
+buck run fbcode//cinderx:python -- \
+    $srcdir/Tools/cases_generator/generate_cases.py \
+        --emit-line-directives \
+        -o Includes/generated_cases.c.h \
+        -m Includes/opcode_metadata.h \
+        Includes/bytecodes.c
+
+gen='generated'
+sed -i "1i // @$gen" Includes/generated_cases.c.h
+sed -i "1i // @$gen" Includes/opcode_metadata.h
