294ad079642e342cc61809cfab1a0e93cf851f82
Alex Malyshev <alexanderm@meta.com>
2024-09-12 07:21:21 -0700

    Separate out CompiledFunction{,Debug} to its own library
    Summary:
    By combining it with Compiler, it means that everything that uses
    CompiledFunction needs to pull in all of Jit/codegen, which shouldn't be
    necessary and leads to dependency cycles.
    
    Reviewed By: SonicField
    
    Differential Revision: D62448594
    
    fbshipit-source-id: f196b03a48316f2632eafce0c1e1ee97a088a4c5

--- Jit/compiler.cpp
+++ Jit/compiler.cpp
@@ -5,7 +5,6 @@
 #include "cinderx/Common/log.h"
 
 #include "cinderx/Jit/config.h"
-#include "cinderx/Jit/disassembler.h"
 #include "cinderx/Jit/hir/analysis.h"
 #include "cinderx/Jit/hir/builder.h"
 #include "cinderx/Jit/hir/optimization.h"
@@ -14,33 +13,12 @@
 #include "cinderx/Jit/hir/ssa.h"
 #include "cinderx/Jit/jit_time_log.h"
 
-#include <Python.h>
 #include <nlohmann/json.hpp>
 
 #include <chrono>
 #include <fstream>
 
 namespace jit {
-
-void CompiledFunction::disassemble() const {
-  JIT_ABORT("disassemble() cannot be called in a release build.");
-}
-
-void CompiledFunction::printHIR() const {
-  JIT_ABORT("printHIR() cannot be called in a release build.");
-}
-
-void CompiledFunctionDebug::disassemble() const {
-  jit::disassemble(
-      reinterpret_cast<const char*>(vectorcallEntry()),
-      codeSize(),
-      reinterpret_cast<vma_t>(vectorcallEntry()));
-}
-
-void CompiledFunctionDebug::printHIR() const {
-  jit::hir::HIRPrinter printer;
-  printer.Print(*irfunc_.get());
-}
 
 struct PassTimer {
   explicit PassTimer() : start(std::chrono::steady_clock::now()) {}
--- Jit/compiler.h
+++ Jit/compiler.h
@@ -5,9 +5,9 @@
 #include "cinderx/Common/util.h"
 
 #include "cinderx/Jit/codegen/gen_asm.h"
+#include "cinderx/Jit/compiled_function.h"
 #include "cinderx/Jit/hir/hir.h"
 #include "cinderx/Jit/hir/preload.h"
-#include "cinderx/Jit/runtime.h"
 
 #include <Python.h>
 
@@ -17,113 +17,10 @@
 
 namespace jit {
 
-// CompiledFunction contains the native code that was compiled for a python
-// function
-//
-// It is responsible for managing the lifetime of the executable memory and
-// binding the lifetime of anything it depends on to it.
-class CompiledFunction {
- public:
-  CompiledFunction(
-      std::span<const std::byte> code,
-      vectorcallfunc vectorcall_entry,
-      void* static_entry,
-      CodeRuntime* code_runtime,
-      int stack_size,
-      int spill_stack_size,
-      hir::Function::InlineFunctionStats inline_function_stats,
-      const hir::OpcodeCounts& hir_opcode_counts)
-      : code_(code),
-        vectorcall_entry_(vectorcall_entry),
-        static_entry_(static_entry),
-        code_runtime_(code_runtime),
-        stack_size_(stack_size),
-        spill_stack_size_(spill_stack_size),
-        inline_function_stats_(std::move(inline_function_stats)),
-        hir_opcode_counts_(hir_opcode_counts) {}
+using PostPassFunction = std::function<
+    void(hir::Function& func, const char* pass_name, std::size_t time_ns)>;
 
-  virtual ~CompiledFunction() {}
-
-  // Get the buffer containing the compiled machine code.  The start of this
-  // buffer is not guaranteed to be a valid entry point.
-  std::span<const std::byte> codeBuffer() const {
-    return code_;
-  }
-
-  vectorcallfunc vectorcallEntry() const {
-    return vectorcall_entry_;
-  }
-
-  void* staticEntry() const {
-    return static_entry_;
-  }
-
-  PyObject* invoke(PyObject* func, PyObject** args, Py_ssize_t nargs) const {
-    return vectorcall_entry_(func, args, nargs, NULL);
-  }
-
-  virtual void printHIR() const;
-  virtual void disassemble() const;
-
-  CodeRuntime* codeRuntime() const {
-    return code_runtime_;
-  }
-
-  size_t codeSize() const {
-    return code_.size();
-  }
-
-  int stackSize() const {
-    return stack_size_;
-  }
-
-  int spillStackSize() const {
-    return spill_stack_size_;
-  }
-
-  const hir::Function::InlineFunctionStats& inlinedFunctionsStats() const {
-    return inline_function_stats_;
-  }
-
-  const hir::OpcodeCounts& hirOpcodeCounts() const {
-    return hir_opcode_counts_;
-  }
-
- private:
-  DISALLOW_COPY_AND_ASSIGN(CompiledFunction);
-
-  const std::span<const std::byte> code_;
-  vectorcallfunc const vectorcall_entry_;
-  void* const static_entry_;
-  CodeRuntime* const code_runtime_;
-  const int stack_size_;
-  const int spill_stack_size_;
-  hir::Function::InlineFunctionStats inline_function_stats_;
-  hir::OpcodeCounts hir_opcode_counts_;
-};
-
-// Same as CompiledFunction but keeps the HIR function around for debugging.
-class CompiledFunctionDebug : public CompiledFunction {
- public:
-  template <typename... Args>
-  explicit CompiledFunctionDebug(
-      std::unique_ptr<hir::Function> irfunc,
-      Args&&... args)
-      : CompiledFunction{std::forward<Args>(args)...},
-        irfunc_{std::move(irfunc)} {}
-
-  void disassemble() const override;
-  void printHIR() const override;
-
- private:
-  std::unique_ptr<hir::Function> irfunc_;
-};
-
-typedef std::function<
-    void(hir::Function& func, const char* pass_name, std::size_t time_ns)>
-    PostPassFunction;
-
-// Control what compiler passes are run.
+// Controls what compiler passes are run.
 enum PassConfig : uint64_t {
   // Only run compiler passes that are necessary for correctness, e.g. SSAify.
   kMinimal = 0,
@@ -148,8 +45,7 @@ enum PassConfig : uint64_t {
   kAllExceptInliner = kAll & ~kInliner,
 };
 
-// Compiler is the high-level interface for translating Python functions into
-// native code.
+// The high-level interface for translating Python functions into native code.
 class Compiler {
  public:
   Compiler() = default;
@@ -163,6 +59,7 @@ class Compiler {
 
   // Runs all the compiler passes on the HIR function.
   static void runPasses(hir::Function&, PassConfig config);
+
   // Runs the compiler passes, calling callback on the HIR function after each
   // pass.
   static void runPasses(
