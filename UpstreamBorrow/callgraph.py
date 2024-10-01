c8a30e93da1442395b8e9e836486099c2aea4c86
Martin DeMello <martindemello@meta.com>
2024-09-30 18:38:59 -0700

    Find missing dependencies from cinderx.
    Summary:
    - Add a find_missing.py script to scan the symbol tables for libcinderx and
      libpython, and return undefined symbols from cinderx that are not exported by
      libpython.
    - Modify callgraph.py to read the json output from find_missing.py
    
    The final output is a combined list of borrow directives that can be pasted
    into borrowed.template.c
    
    Reviewed By: alexmalyshev
    
    Differential Revision: D63408520
    
    fbshipit-source-id: aa614d5c71c59af8b5d61447e0943e87692affa4

--- UpstreamBorrow/callgraph.py
+++ UpstreamBorrow/callgraph.py
@@ -1,3 +1,5 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+
 # pyre-strict
 import json
 import os
@@ -10,11 +12,11 @@ from clang.cindex import CursorKind
 
 from .clang_parser import FileParser, ParsedFile
 
-CallGraph = dict[str, list[str]]
+Graph = dict[str, list[str]]
 
 
-def make_callgraph(parsed_file: ParsedFile) -> CallGraph:
-    graph = defaultdict(list)
+def make_callgraph(parsed_file: ParsedFile) -> Graph:
+    graph = {}
     for cursor in parsed_file.translation_unit.cursor.walk_preorder():
         if not (
             # pyre-ignore[16]: `CursorKind` has no attribute `FUNCTION_DECL`.
@@ -23,6 +25,7 @@ def make_callgraph(parsed_file: ParsedFile) -> CallGraph:
         ):
             continue
         func_name = cursor.spelling
+        graph[func_name] = []
         for node in cursor.walk_preorder():
             # pyre-ignore[16]: `CursorKind` has no attribute `CALL_EXPR`.
             if node.kind == CursorKind.CALL_EXPR and node.referenced:
@@ -30,7 +33,7 @@ def make_callgraph(parsed_file: ParsedFile) -> CallGraph:
     return graph
 
 
-def filter_graph(graph: CallGraph) -> CallGraph:
+def filter_graph(graph: Graph) -> Graph:
     """Filter the graph to calls within the same file."""
     out = {}
     for k, v in graph.items():
@@ -38,21 +41,21 @@ def filter_graph(graph: CallGraph) -> CallGraph:
     return out
 
 
-def dep_list(graph: CallGraph, fn: str) -> list[str]:
-    """Get the transitive closure of a function's dependencies."""
-    # TODO: The algorithm won't get the ordering right if we have a recursive
-    # cycle; fix it if it becomes an issue.
-    queue = deque([fn])
-    seen = set()
-    out = []
-    while queue:
-        f = queue.popleft()
-        if f not in seen:
-            seen.add(f)
-            out.append(f)
-            if f in graph:
-                queue.extend(graph[f])
-    out.reverse()
+def get_ordered_deps(graph: Graph, nodes: list[str]) -> list[str]:
+    """Find and topologically sort a combined dep list for nodes."""
+    visited: dict[str, bool] = {node: False for node in graph}
+    out: list[str] = []
+
+    def dfs(node: str) -> None:
+        if visited[node]:
+            return
+        visited[node] = True
+        for neighbor in graph[node]:
+            dfs(neighbor)
+        out.append(node)
+
+    for node in nodes:
+        dfs(node)
     return out
 
 
@@ -64,31 +67,49 @@ def resource_path(relpath: str) -> str:
     return str(resources.files(__package__).joinpath(relpath))
 
 
-@click.command()
-@click.argument("input_file", type=click.Path())
-@click.argument("entry_point", type=click.Path())
-@click.argument("output_file", type=click.Path())
-@click.option("-p", "--py_version", default="3.12")
-@click.option("-b", "--borrow/--no-borrow", default=False)
-def main(
-    input_file: str,
-    entry_point: str,
-    output_file: str,
-    py_version: str,
-    borrow: bool,
-) -> None:
-    print(f"Initial directory: {os.getcwd()}")
+class FilteredCallGraph:
+    file: ParsedFile
+    graph: Graph
 
-    compile_commands = resource_path("compilation-database")
-    output_file = abspath(output_file)
-    fp = FileParser(compile_commands)
+    def __init__(self, file: ParsedFile) -> None:
+        self.file = file
+        self.graph = filter_graph(make_callgraph(file))
+
+    def deps(self, fns: list[str]) -> list[str]:
+        """Take a list of functions and calculate all their dependencies."""
+        return get_ordered_deps(self.graph, fns)
+
+
+def print_borrows(
+    fp: FileParser, input_file: str, fns: list[str], py_version: str
+) -> None:
     parsed_file = fp.parse(input_file)
-    callgraph = make_callgraph(parsed_file)
-    callgraph = filter_graph(callgraph)
-    deps = dep_list(callgraph, entry_point)
-    with open(output_file, "w") as f:
-        json.dump(deps, f)
+    callgraph = FilteredCallGraph(parsed_file)
+    deps = callgraph.deps(fns)
     # Spit out borrow commands to stdout
-    if borrow:
-        for dep in deps:
-            print(f"// @Borrow function {dep} from {input_file} [{py_version}]")
+    for dep in deps:
+        print(f"// @Borrow function {dep} from {input_file} [{py_version}]")
+
+
+@click.command()
+@click.option("-p", "--py_version", default="3.12")
+@click.argument("input_file", type=click.Path())
+@click.argument("entry_point", required=False)
+def main(input_file: str, entry_point: str | None, py_version: str) -> None:
+    compile_commands = resource_path("compilation-database")
+    fp = FileParser(compile_commands)
+
+    # If we have an entry point, the input is a filepath relative to the
+    # cpython source directory.
+    if entry_point:
+        print_borrows(fp, input_file, [entry_point], py_version)
+        return
+
+    # If not, the input is a json file containing a dict of files to entry
+    # points from which we generate a combined borrow list. See
+    # `find_missing.py` for the script that generates the json file.
+    with open(input_file, "r") as f:
+        files = json.load(f)
+    for file, fns in files.items():
+        print_borrows(fp, file, fns, py_version)
+        print()
---
+++ UpstreamBorrow/find_missing.py
@@ -0,0 +1,128 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+
+# Find symbols referenced in cinderx that are not exported by cpython.
+#
+# Helper script for upstream borrowing; the output of find_missing.py will be
+# processed by callgraph.py, and the output of that pasted into
+# borrowed.template.c
+#
+# NOTE: This has no dependencies, and can be run via buck run or simply as
+#   $ fbpython find_missing.py missing.json
+
+# pyre-strict
+import json
+import os
+import os.path
+import random
+import subprocess
+import sys
+from collections import defaultdict
+from typing import Iterable
+
+
+# pyre-ignore[2]: Parameter `**kwargs` has no type specified.
+def run(cmd: list[str], **kwargs) -> str:
+    """Capture stdout, let stderr be displayed to show progress."""
+    result = subprocess.Popen(cmd, text=True, stdout=subprocess.PIPE, **kwargs)
+    ret, _ = result.communicate()
+    return ret
+
+
+def build(target: str) -> str:
+    cmd = ["buck", "build", "-c", "cinderx.use_3_12=true", target, "--show-full-output"]
+    return run(cmd).strip().split(" ")[-1]
+
+
+def is_python_symbol(s: str) -> bool:
+    return (" Py" in s) or (" _Py" in s)
+
+
+def get_python_symbols(output: str) -> list[str]:
+    lines = output.split("\n")
+    return [x for x in lines if is_python_symbol(x)]
+
+
+def get_cinderx_undef() -> set[str]:
+    sh_file = build("fbcode//cinderx:python")
+    d = os.path.dirname(os.path.dirname(sh_file))
+    so_file = os.path.join(
+        d,
+        "PythonBin/__python__/out/install/lib/python3.12/lib-dynload/",
+        "_cinderx.cpython-312-x86_64-linux-gnu.so",
+    )
+    undef = run(["nm", "-u", "--demangle", so_file])
+    return {x.lstrip(" U") for x in get_python_symbols(undef)}
+
+
+def get_cpython_exported() -> set[str]:
+    so_file = build("fbsource//third-party/python/3.12:libpython[shared]")
+    exported = run(["nm", "--dynamic", "--defined", so_file])
+    exported = [x.split(" ", 3)[-1] for x in get_python_symbols(exported)]
+    return set(exported)
+
+
+def get_fbsource_root() -> str:
+    try:
+        fbsource_root = subprocess.run(
+            ["hg", "root"], capture_output=True, encoding="utf-8", check=True
+        ).stdout.strip()
+    except subprocess.CalledProcessError as e:
+        raise Exception(
+            f"Failing stdout:\n{e.stdout}\nFailing stderr:\n{e.stderr}\n"
+        ) from e
+    return fbsource_root
+
+
+def find_py_symbols(syms: Iterable[str]) -> dict[str, list[str]]:
+    # grep through the cpython source for the definition of each symbol.
+    # We take advantage of the fact that the code is formatted like
+    # void
+    # fn(args)
+    # {
+    # so we can search for "^symbol" to distinguish definitions from function
+    # calls.
+
+    patfile = "/tmp/patterns-" + str(random.randint(0, 1024))
+    with open(patfile, "w") as f:
+        for sym in syms:
+            f.write(f"^{sym}\\b\n")
+    # We need to grep over the cpython base dir
+    path = os.path.join(get_fbsource_root(), "third-party/python/3.12")
+    ret = run(["grep", "-oHR", "-f", patfile, path])
+    out = defaultdict(list)
+    seen = set()  # make sure each symbol has a single definition
+    for line in ret.strip().split("\n"):
+        file, sym = line.split(":")
+        if not file.endswith(".c"):
+            continue
+        if sym in seen:
+            raise RuntimeError(f"Multiple definitions found for {sym}")
+        seen.add(sym)
+        out[file.lstrip("./")].append(sym)
+    return out
+
+
+def find_missing() -> dict[str, list[str]]:
+    undef = get_cinderx_undef()
+    exported = get_cpython_exported()
+    missing = undef - exported
+    srcs = find_py_symbols(missing)
+    return srcs
+
+
+def main() -> None:
+    missing = find_missing()
+    if len(sys.argv) == 2:
+        outfile = sys.argv[1]
+        with open(outfile, "w") as f:
+            json.dump(missing, f)
+        print(f"Wrote missing symbols to {outfile}")
+    else:
+        for f, s in missing.items():
+            print(f"{f}:")
+            for sym in s:
+                print(f"  {sym}")
+
+
+if __name__ == "__main__":
+    main()
