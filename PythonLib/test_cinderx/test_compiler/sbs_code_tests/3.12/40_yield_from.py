1550734af066e55e207b13a068be2aa1128dda21
Dino Viehland <dinoviehland@meta.com>
2024-10-23 12:56:38 -0700

    Split YIELD_FROM
    Summary:
    This brings back D64269471 unmodified.
    
    `YIELD_FROM` is now implemented in the form of a loop in the byte code until an exception happens, so the compiler does a `SEND` emits a `SETUP_FINALLY`, does `YIELD_VALUE`, and then jumps back to the `SEND.
    
    This is easy enough to emit, but we have problems with the code-gen. `SETUP_FINALLY` is now a pseudo instruction so we basically just need to ignore that in the `pyassem` level right now. Later we'll implement the exception handling regions and those hacks can go away.
    
    Reviewed By: alexmalyshev
    
    Differential Revision: D64720556
    
    fbshipit-source-id: 005b5927d7bb4e0b5155496a52bdc9311b4baaef

---
+++ PythonLib/test_cinderx/test_compiler/sbs_code_tests/3.12/40_yield_from.py
@@ -0,0 +1,24 @@
+# pyre-ignore-all-errors
+def f():
+    yield from g
+# EXPECTED:
+[
+    ...,
+    CODE_START('f'),
+    ...,
+    LOAD_GLOBAL('g'),
+    GET_YIELD_FROM_ITER(0),
+    LOAD_CONST(None),
+    SEND(Block(3)),
+    ...,
+    YIELD_VALUE(0),
+    ...,
+    RESUME(2),
+    JUMP_BACKWARD_NO_INTERRUPT(Block(1)),
+
+    # Currently differs from 3.12 because we don't yet push cold blocks to the end
+    CLEANUP_THROW(0),
+    END_SEND(0),
+    POP_TOP(0),
+    RETURN_CONST(None),
+]
