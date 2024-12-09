d96dcf19fa4ce62ca090954da13030b288f198b4
Alex Malyshev <alexanderm@meta.com>
2024-12-09 07:26:07 -0800

    Re-enable multithreaded compile test
    Summary:
    This was disabled when we moved to fbcode/ because we couldn't get TSAN properly
    configured.  We should still be running the test though, just in case it exposes
    data races in other ways.
    
    Reviewed By: jbower-fb
    
    Differential Revision: D66884178
    
    fbshipit-source-id: cdce82b54f7e0e0cbc4c08b65bc16fda3be3e30a

---
+++ TestScripts/test_multithreaded_compile.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -xe
+
+WORKERS=10
+
+# Running the test module straight from PythonLib/
+cd "$(dirname "$(readlink -f "$0")")"/../PythonLib
+
+function run() {
+    local mode=$1
+    buck2 run @fbcode//mode/"${mode}" fbcode//cinderx:python -- -X jit -X jit-multithreaded-compile-test -X jit-batch-compile-workers="${WORKERS}" -m test_cinderx.multithreaded_compile_test
+}
+
+# TODO(T186691817): There should be at least one run with TSAN but we don't have
+# that configured properly in buck yet.
+run dbg
+run dev
+run opt
