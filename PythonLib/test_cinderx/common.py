fd80cca88d166256eab5feda01e76f5ec402391b
Alex Malyshev <alexanderm@meta.com>
2025-03-14 09:35:37 -0700

    Move JIT exception tests into separate module, enable buck
    Summary:
    Getting coverage across multiple configurations of JIT/noJIT, 3.10/3.12.
    Simplifies test_cinderjit.py which is too damn big.
    
    Reviewed By: SonicField
    
    Differential Revision: D70101750
    
    fbshipit-source-id: c7e1d0c91ff02b02c7c7a957c216b48de1e2f8cd

--- PythonLib/test_cinderx/common.py
+++ PythonLib/test_cinderx/common.py
@@ -1,5 +1,7 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 
+import dis
+
 # Decorator to return a new version of the function with an alternate globals
 # dict.
 def with_globals(gbls):
@@ -10,5 +12,26 @@ def with_globals(gbls):
         new_func.__module__ = func.__module__
         new_func.__kwdefaults__ = func.__kwdefaults__
         return new_func
+
+    return decorator
+
+
+def failUnlessHasOpcodes(*required_opnames):
+    """Fail a test unless func has all of the opcodes in `required` in its code
+    object.
+    """
+
+    def decorator(func):
+        opnames = {i.opname for i in dis.get_instructions(func)}
+        missing = set(required_opnames) - opnames
+        if missing:
+
+            def wrapper(*args):
+                raise AssertionError(
+                    f"Function {func.__qualname__} missing required opcodes: {missing}"
+                )
+
+            return wrapper
+        return func
 
     return decorator
