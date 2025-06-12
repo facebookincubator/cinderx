5bb106b42c3f10bea2dfff8362464c9488026fde
Alex Malyshev <alexanderm@meta.com>
2025-06-12 11:03:18 -0700

    Don't use cinder/ci_api.h in cinderx
    Summary:
    It's found only in the Meta Python runtime, and all we're using it for is the
    CiAPI_DATA macro.  We can just as easily use the PyAPI_DATA macro for that.
    
    Reviewed By: jbower-fb
    
    Differential Revision: D76448883
    
    fbshipit-source-id: 2d58c4632cf5e132fe2fa989ff588f5f57358f02

--- Interpreter/Includes/cinder/hooks.h
+++ Interpreter/Includes/cinder/hooks.h
@@ -6,12 +6,11 @@
 extern "C" {
 #endif
 
-#include "cinder/ci_api.h"
-#include "pystate.h" // PyThreadState
+#include "Python.h"
 
 /* Hooks needed by CinderX that have not been added to upstream. */
 
-CiAPI_DATA(_PyFrameEvalFunction) Ci_hook_EvalFrame;
+PyAPI_DATA(_PyFrameEvalFunction) Ci_hook_EvalFrame;
 
 #ifdef __cplusplus
 } // extern "C"
