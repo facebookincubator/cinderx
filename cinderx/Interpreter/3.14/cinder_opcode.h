9f3bd12cf1220aa8b5a28fb7d5fa9a9b383e80b7
Jacob Bower <jbower@meta.com>
2025-10-14 14:27:04 -0700

    Bring back CinderX-local definitions of bytcode tables
    Summary: Without these some modes of build fail.
    
    Reviewed By: mpage
    
    Differential Revision: D84561456
    
    fbshipit-source-id: 25e131f49e8493cc6c280ca8aa82e568c14287f3

--- cinderx/Interpreter/3.14/cinder_opcode.h
+++ cinderx/Interpreter/3.14/cinder_opcode.h
@@ -1,6 +1,16 @@
 // Copyright (c) Meta Platforms, Inc. and affiliates.
 
 #include "cinderx/python.h"
+
+#define _PyOpcode_num_popped _CiOpcode_num_popped
+#define _PyOpcode_num_pushed _CiOpcode_num_pushed
+#define _PyOpcode_opcode_metadata _CiOpcode_opcode_metadata
+#define _PyOpcode_macro_expansion _CiOpcode_macro_expansion
+#define _PyOpcode_OpName _CiOpcode_OpName
+#define _PyOpcode_Caches _CiOpcode_Caches
+#define _PyOpcode_Deopt _CiOpcode_Deopt
+#define _PyOpcode_PseudoTargets _CiOpcode_PseudoTargets
+
 #include "internal/pycore_opcode_metadata.h"
 
 #ifndef Ci_INTERNAL_OPCODE_H
@@ -12,6 +22,4 @@
 
 #include "cinderx/Interpreter/cinder_opcode_ids.h"
 
-#define _CiOpcode_Deopt _PyOpcode_Deopt
-#define _CiOpcode_Caches _PyOpcode_Caches
 #endif
