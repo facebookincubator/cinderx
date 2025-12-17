420dfaa0bbf330fab9adb59b26ebfc2ce39c5ac1
Dino Viehland <dinoviehland@meta.com>
2025-12-16 16:04:58 -0800

    Enable 3.15 builds and tests in CI
    Summary: Enables CI on 3.15 now that buckification is fully landed. The buckified unit tests don't currently run because native Python isn't supported so instead we have the CinderX tests enabled via our CI definitions.
    
    Reviewed By: alexmalyshev
    
    Differential Revision: D86703651
    
    fbshipit-source-id: 61e864ee5b838d0d9f0663682a86ce7728b6cb6d

--- cinderx/TestScripts/test_test_defs.sh
+++ cinderx/TestScripts/test_test_defs.sh
@@ -7,9 +7,14 @@ set -e
 
 cd "$(dirname "$(readlink -f "$0")")"/../
 
+echo Generate 3.10
 buck run fbcode//cinderx:ctl3.10 > /tmp/3.10.txt
+echo Generate 3.12
 buck run fbcode//cinderx:ctl3.12 > /tmp/3.12.txt
+echo Generate 3.14
 buck run fbcode//cinderx:ctl3.14 > /tmp/3.14.txt
+echo Generate 3.15
+buck run fbcode//cinderx:ctl3.15 > /tmp/3.15.txt
 
 {
     echo "TESTS = {"
@@ -21,6 +26,9 @@ buck run fbcode//cinderx:ctl3.14 > /tmp/3.14.txt
     echo ","
     echo \"3.14\":
     cat /tmp/3.14.txt
+    echo ","
+    echo \"3.15\":
+    cat /tmp/3.15.txt
     echo "}"
 } > /tmp/tests.txt
 pyfmt /tmp/tests.txt
