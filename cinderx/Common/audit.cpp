5284dd11e2911262611e13015c09b2947d4d4a5e
Dino Viehland <dinoviehland@meta.com>
2026-01-09 10:56:32 -0800

    Don't read _PyRuntime struct if the size has changed
    Summary:
    We have some code that attempts to dig into _PyRuntime to verify our audit hook was successfully installed. But _PyRuntime's size can change between minor versions, so we can't safely read this if it's size has changed since we were build.
    
    Let's skip the due-diligence phase of installing the audit hook if the size has changed.
    
    Reviewed By: czardoz
    
    Differential Revision: D90340239
    
    fbshipit-source-id: 276eb44dcfc9c5b8f5643e187baf9b8293c0e8e2

--- cinderx/Common/audit.cpp
+++ cinderx/Common/audit.cpp
@@ -16,6 +16,13 @@ bool installAuditHook(Py_AuditHookFunction func, void* userData) {
   }
 
   _PyRuntimeState* runtime = &_PyRuntime;
+#if PY_VERSION_HEX >= 0x030E0000
+  // If the actual runtime state is a different size than we were compiled with
+  // we cannot safely do the check below.
+  if (runtime->debug_offsets.runtime_state.size != sizeof(_PyRuntimeState)) {
+    return true;
+  }
+#endif
   _Py_AuditHookEntry* audit_hook_head =
 #if PY_VERSION_HEX < 0x030C0000
       runtime->audit_hook_head
