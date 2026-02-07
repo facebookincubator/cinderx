6677d069bbbf890943d4daaf80447813217114d3
Dino Viehland <dinoviehland@meta.com>
2026-02-06 18:35:12 -0800

    Provide improved diagnostics on failed CinderX initialization
    Summary: When we fail to initialize `cinderx` it's usually due to a missing symbol - but it's hard to know what that is right now. This tries to import `_cinderx` and then prints the exception so it's more actionable.
    
    Reviewed By: yoney
    
    Differential Revision: D92569204
    
    fbshipit-source-id: d6f8189f3c59be09f139bdfcaee3f248803c1e5d

--- cinderx/PythonLib/test_cinderx/test_oss_quick.py
+++ cinderx/PythonLib/test_cinderx/test_oss_quick.py
@@ -10,6 +10,12 @@ class CinderXOSSTest(unittest.TestCase):
     def test_import(self) -> None:
         import cinderx  # noqa: F401
 
+        if not cinderx.is_initialized():
+            try:
+                import _cinderx
+            except Exception as e:
+                print(f"Failed to import cinder: {e}")
+
         self.assertTrue(cinderx.is_initialized())
 
 
