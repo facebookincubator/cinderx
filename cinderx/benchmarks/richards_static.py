67534431e4abd24b720d6746c0ebfce52c1ad816
Alex Malyshev <alexanderm@meta.com>
2026-02-11 14:47:24 -0800

    Adding static richards benchmark(s)
    Summary: Adding this in for comparison.
    
    Reviewed By: yoney
    
    Differential Revision: D92902584
    
    fbshipit-source-id: 0ce8c98ff8cdf78ec233c4835f4ed396856e5c35

---
+++ cinderx/benchmarks/richards_static.py
@@ -0,0 +1,18 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+
+import sys
+
+import cinderx.jit
+from cinderx.compiler.strict import loader as static_python_loader
+
+from .richards_static_lib import Richards
+
+
+if __name__ == "__main__":
+    static_python_loader.install()
+    cinderx.jit.auto()
+
+    num_iterations = 1
+    if len(sys.argv) > 1:
+        num_iterations = int(sys.argv[1])
+    Richards().run(num_iterations)
