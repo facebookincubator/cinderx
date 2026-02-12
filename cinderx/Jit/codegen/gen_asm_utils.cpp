e19f714ca4fc5cb0ed92380ff1fffa3083c58a4b
Kevin Newton <kddnewton@meta.com>
2026-02-11 18:01:17 -0800

    Support aarch64 in gen_asm_utils.cpp
    Summary: Support aarch64 when generating calls to labels or absolute addresses in gen_asm_utils.cpp.
    
    Reviewed By: mpage
    
    Differential Revision: D90511759
    
    fbshipit-source-id: 95a69d552a03331d5bdcdd649010cc64a9786fb7

--- cinderx/Jit/codegen/gen_asm_utils.cpp
+++ cinderx/Jit/codegen/gen_asm_utils.cpp
@@ -24,19 +24,28 @@ void emitCall(
     const jit::lir::Instruction* instr) {
 #if defined(CINDER_X86_64)
   env.as->call(label);
-  recordDebugEntry(env, instr);
+#elif defined(CINDER_AARCH64)
+  env.as->bl(label);
 #else
   CINDER_UNSUPPORTED
 #endif
+  recordDebugEntry(env, instr);
 }
 
 void emitCall(Environ& env, uint64_t func, const jit::lir::Instruction* instr) {
 #if defined(CINDER_X86_64)
   env.as->call(func);
-  recordDebugEntry(env, instr);
+#elif defined(CINDER_AARCH64)
+  // Note that we could do better than this if asmjit knew how to handle arm64
+  // relocations for relative calls. That work is done in
+  // https://github.com/asmjit/asmjit/issues/499, but as of writing is not yet
+  // available.
+  env.as->mov(arch::reg_scratch_br, func);
+  env.as->blr(arch::reg_scratch_br);
 #else
   CINDER_UNSUPPORTED
 #endif
+  recordDebugEntry(env, instr);
 }
 
 } // namespace jit::codegen
