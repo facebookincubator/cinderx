c97dfc9c27573c193a27707d5e3dd4ad96c2f88d
Alex Malyshev <alexanderm@meta.com>
2026-03-25 10:16:28 -0700

    spelling: fix typo "relavant" -> "relevant" in gen_opcodes_314.py
    Summary:
    Fix spelling mistake in a comment in gen_opcodes_314.py. The word
    "relavant" should be "relevant".
    
    Reviewed By: mpage
    
    Differential Revision: D97966081
    
    fbshipit-source-id: 02fc76020088f9e0ae010ccdbd05346edc5b2a61

--- cinderx/Interpreter/gen_opcodes_314.py
+++ cinderx/Interpreter/gen_opcodes_314.py
@@ -23,7 +23,7 @@ opcode.init(
 template = """
 // Copyright (c) Meta Platforms, Inc. and affiliates.
 
-// 3.14 has a simple file that just defines the relavant ids:
+// 3.14 has a simple file that just defines the relevant ids:
 
 #include "opcode.h"
 
