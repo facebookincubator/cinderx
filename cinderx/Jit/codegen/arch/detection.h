e91141f17d5269811dcbcb45e5d74b2dbab2efcd
Alex Malyshev <alexanderm@meta.com>
2026-04-06 08:57:30 -0700

    Add Arch enum class and kBuildArch constexpr to detection.h
    Summary:
    Expose the build target architecture as a typed enum and compile-time
    constant so codegen code can query it without preprocessor guards.
    
    Reviewed By: yoney
    
    Differential Revision: D99491341
    
    fbshipit-source-id: ead21b6a575b8e5bf1255d9ddd5963c45594cb10

--- cinderx/Jit/codegen/arch/detection.h
+++ cinderx/Jit/codegen/arch/detection.h
@@ -2,19 +2,33 @@
 
 #pragma once
 
+namespace jit::codegen::arch {
+
+// The CPU architecture targeted by the current build.
+enum class Arch {
+  kX86_64,
+  kAarch64,
+  kUnknown,
+};
+
 // This macro is a marker for places that need platform-specific code.
 #define CINDER_UNSUPPORTED
 
 #if defined(__x86_64__)
 
 #define CINDER_X86_64
+constexpr Arch kBuildArch = Arch::kX86_64;
 
 #elif defined(__aarch64__)
 
 #define CINDER_AARCH64
+constexpr Arch kBuildArch = Arch::kAarch64;
 
 #else
 
 #define CINDER_UNKNOWN
+constexpr Arch kBuildArch = Arch::kUnknown;
 
 #endif
+
+} // namespace jit::codegen::arch
