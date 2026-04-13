400fa18900e9d65098cd05cdb22574e2a3d50289
Dino Viehland <dinoviehland@meta.com>
2026-04-13 15:54:41 -0700

    Remove 3.10 test defs
    Summary: Our 3.10 build definitions are gone, this gets rid of our test definitions as ctl3.10 is no longer available.
    
    Reviewed By: alexmalyshev
    
    Differential Revision: D100673341
    
    fbshipit-source-id: acd20f93f90fbedd6ec25703333d5f432d6ca6a2

--- cinderx/TestScripts/gen_test_defs.sh
+++ cinderx/TestScripts/gen_test_defs.sh
@@ -18,8 +18,6 @@ declare -A TSAN_VERSIONS=(
     # ["3.15t"]="main"
 )
 
-echo Generate 3.10
-buck run fbcode//cinderx:ctl3.10 > /tmp/3.10.txt
 echo Generate 3.12
 buck run fbcode//cinderx:ctl3.12 > /tmp/3.12.txt
 echo Generate 3.14
@@ -51,9 +49,6 @@ done
     echo "}"
     echo ""
     echo "TESTS = {"
-    echo \"3.10\":
-    cat /tmp/3.10.txt
-    echo ","
     echo \"3.12\":
     cat /tmp/3.12.txt
     echo ","
