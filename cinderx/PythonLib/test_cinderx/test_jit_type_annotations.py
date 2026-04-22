00c98e8c046c3afbf833f99660156a5c2c679b2a
generatedunixname923199350665076 <noreply+923199350665076@fb.com>
2026-04-22 13:11:01 -0700

    PYRE_STRICT in test_jit_type_annotations.py
    Reviewed By: alexmalyshev
    
    Differential Revision: D101975449
    
    fbshipit-source-id: ce0e72e4ca32ed6a3466db236ed9d078636f0f39

--- cinderx/PythonLib/test_cinderx/test_jit_type_annotations.py
+++ cinderx/PythonLib/test_cinderx/test_jit_type_annotations.py
@@ -1,9 +1,9 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 
-# pyre-unsafe
+# pyre-strict
 
 import unittest
-from typing import Any, Generator
+from typing import Any, Generator, Self
 
 import cinderx
 import cinderx.jit
@@ -13,22 +13,22 @@ from cinderx.test_support import passUnless
 @passUnless(cinderx.jit.is_enabled(), "JIT required")
 class TypeAnnotationTests(unittest.TestCase):
     class Box:
-        def __init__(self, value):
+        def __init__(self, value: int) -> None:
             self.value = value
 
-        def __add__(self, other):
+        def __add__(self, other: int) -> Self:
             return self.__class__(self.value + other)
 
-        def __eq__(self, other):
+        def __eq__(self, other: object) -> bool:
             return isinstance(other, self.__class__) and self.value == other.value
 
-    def setUp(self):
+    def setUp(self) -> None:
         cinderx.jit.enable_emit_type_annotation_guards()
 
-    def tearDown(self):
+    def tearDown(self) -> None:
         cinderx.jit.disable_emit_type_annotation_guards()
 
-    def test_good_simple(self):
+    def test_good_simple(self) -> None:
         def f(x: int) -> int:
             return x + 1
 
@@ -38,7 +38,7 @@ class TypeAnnotationTests(unittest.TestCase):
         # pyrefly: ignore [bad-argument-type]
         self.assertIn("LongBinaryOp", cinderx.jit.get_function_hir_opcode_counts(f))
 
-    def test_good_long_list(self):
+    def test_good_long_list(self) -> None:
         def f(
             x1: int, x2: int, x3: int, x4: int, x5: int, x6: int, x7: int, x8: int
         ) -> int:
@@ -48,7 +48,7 @@ class TypeAnnotationTests(unittest.TestCase):
 
         self.assertEqual(f(1, 2, 3, 4, 5, 6, 7, 8), 36)
 
-    def test_good_closure(self):
+    def test_good_closure(self) -> None:
         def f(x: int) -> int:
             def g(y: int) -> int:
                 return x + y
@@ -59,7 +59,7 @@ class TypeAnnotationTests(unittest.TestCase):
 
         self.assertEqual(f(42), 43)
 
-    def test_good_generator(self):
+    def test_good_generator(self) -> None:
         def f(x: int) -> int:
             def g(y: int) -> Generator[int, Any, Any]:
                 yield x + y
@@ -70,7 +70,7 @@ class TypeAnnotationTests(unittest.TestCase):
 
         self.assertEqual(f(42), 43)
 
-    def test_bad(self):
+    def test_bad(self) -> None:
         def f(x: int) -> int:
             return x + 1
 
