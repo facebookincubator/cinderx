242866994e851776e18ba6ee509b3fcdc1d20e4b
Dino Viehland <dinoviehland@meta.com>
2026-05-06 12:50:53 -0700

    Enable imports on Windows
    Summary: Windows is looking decently stable, let's enable it.
    
    Reviewed By: alexmalyshev
    
    Differential Revision: D104072360
    
    fbshipit-source-id: 43dfe6d9f04f9595e2808873015ed13311989008

--- cinderx/PythonLib/cinderx/__init__.py
+++ cinderx/PythonLib/cinderx/__init__.py
@@ -42,7 +42,7 @@ def is_supported_runtime() -> bool:
     native extension.
     """
 
-    if sys.platform not in ("darwin", "linux"):
+    if sys.platform not in ("darwin", "linux", "win32"):
         return False
 
     version = (sys.version_info.major, sys.version_info.minor)
