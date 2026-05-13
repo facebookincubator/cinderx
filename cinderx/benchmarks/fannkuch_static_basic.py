989943647747cb6d86e1b373c1c8d5329ba164d4
Dino Viehland <dinoviehland@meta.com>
2026-05-13 16:46:10 -0700

    Add more benchmarks
    Summary:
    We have these benchmarks from our old Cinder repo:
    https://github.com/facebookincubator/cinder/tree/meta/3.10.cinder/Tools/benchmarks
    
    This isn't all of them but this is the ones that are currently working.
    
    Reviewed By: alexmalyshev
    
    Differential Revision: D105025996
    
    fbshipit-source-id: 7747f2014a1c35e517ccb447fb58351c65db6ac7

---
+++ cinderx/benchmarks/fannkuch_static_basic.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import sys
+
+from cinderx.compiler.strict import loader as static_python_loader
+
+static_python_loader.install()
+
+from fannkuch_static_basic_lib import DEFAULT_ARG, fannkuch
+
+
+if __name__ == "__main__":
+    num_iterations = 1
+    if len(sys.argv) > 1:
+        num_iterations = int(sys.argv[1])
+    for _ in range(num_iterations):
+        res = fannkuch(DEFAULT_ARG)
+        assert res == 30
---
+++ cinderx/benchmarks/nbody_static_basic.py
@@ -0,0 +1,13 @@
+import sys
+
+from cinderx.compiler.strict import loader as static_python_loader
+
+static_python_loader.install()
+
+from nbody_static_basic_lib import bench_nbody, DEFAULT_ITERATIONS, DEFAULT_REFERENCE
+
+if __name__ == "__main__":
+    num_iterations = 5
+    if len(sys.argv) > 1:
+        num_iterations = int(sys.argv[1])
+    bench_nbody(num_iterations, DEFAULT_REFERENCE, DEFAULT_ITERATIONS)
