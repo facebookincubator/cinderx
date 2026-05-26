453859ff0bf889b18852a3d9f598fec8e626a45c
Alex Malyshev <lex.malyshev@gmail.com>
2026-05-26 13:57:01 -0700

    Fix cinderx/PythonLib/test_cinderx/test_compiler/tests.py (#97)
    Summary:
    test_exception_table.py was misspelled, and test_peephole.py does not exist.
    
    Pull Request resolved: https://github.com/facebookincubator/cinderx/pull/97
    
    Reviewed By: yoney
    
    Differential Revision: D106382151
    
    Pulled By: alexmalyshev
    
    fbshipit-source-id: d59c53b26bde03df2dc21b43429736c0ade35031

--- cinderx/PythonLib/test_cinderx/test_compiler/tests.py
+++ cinderx/PythonLib/test_cinderx/test_compiler/tests.py
@@ -7,16 +7,11 @@ from .test_api import ApiTests
 # pyre-ignore[21]: test_code_sbs not found (not included in buck test build)
 from .test_code_sbs import CodeTests
 from .test_errors import ErrorTests, ErrorTestsBuiltin
-
-# pyre-ignore[21]: test_exceptiontable not found (not included in buck test build)
-from .test_exceptiontable import EncodingTests
+from .test_exception_table import EncodingTests
 from .test_flags import FlagTests
 from .test_graph import GraphTests
 from .test_linepos import LinePositionTests
 from .test_optimizer import AstOptimizerTests
-
-# pyre-ignore[21]: test_peephole not found (not included in buck test build)
-from .test_peephole import PeepHoleTests
 from .test_symbols import SymbolVisitorTests
 from .test_unparse import UnparseTests
 from .test_visitor import VisitorTests
