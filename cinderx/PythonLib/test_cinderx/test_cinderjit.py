2a225fe2fee391c6e2abd7bc7166e029b9e18b2a
Alex Malyshev <lex.malyshev@gmail.com>
2026-05-26 08:38:12 -0700

    Split out inliner tests, skip Windows crasher (#94)
    Summary: Pull Request resolved: https://github.com/facebookincubator/cinderx/pull/94
    
    Reviewed By: kddnewton
    
    Differential Revision: D106365975
    
    Pulled By: alexmalyshev
    
    fbshipit-source-id: 7fc8763ad01ae7ceeddb9de483d22d4318ee2a73

--- cinderx/PythonLib/test_cinderx/test_cinderjit.py
+++ cinderx/PythonLib/test_cinderx/test_cinderjit.py
@@ -7,7 +7,6 @@ import subprocess
 import sys
 import tempfile
 import textwrap
-import traceback
 import unittest
 import warnings
 import weakref
@@ -57,48 +56,6 @@ def firstlineno(func):
 
 
 @cinder_support.failUnlessJITCompiled
-def get_stack():
-    z = 1 + 1  # noqa: F841
-    stack = traceback.extract_stack()
-    return stack
-
-
-@cinder_support.failUnlessJITCompiled
-def get_stack_twice():
-    stacks = []
-    stacks.append(get_stack())
-    stacks.append(get_stack())
-    return stacks
-
-
-@cinder_support.failUnlessJITCompiled
-def get_stack2():
-    z = 2 + 2  # noqa: F841
-    stack = traceback.extract_stack()
-    return stack
-
-
-@cinder_support.failUnlessJITCompiled
-def get_stack_siblings():
-    return [get_stack(), get_stack2()]
-
-
-@cinder_support.failUnlessJITCompiled
-def get_stack_multi():
-    stacks = []
-    stacks.append(traceback.extract_stack())
-    z = 1 + 1  # noqa: F841
-    stacks.append(traceback.extract_stack())
-    return stacks
-
-
-@cinder_support.failUnlessJITCompiled
-def call_get_stack_multi():
-    x = 1 + 1  # noqa: F841
-    return get_stack_multi()
-
-
-@cinder_support.failUnlessJITCompiled
 def func_to_be_inlined(x, y):
     return x + y
 
@@ -121,22 +78,6 @@ def func():
     return a + b + c
 
 
-@cinder_support.failUnlessJITCompiled
-def func_with_defaults_that_will_change(x=1, y=2):
-    return x + y
-
-
-@cinder_support.failUnlessJITCompiled
-def change_defaults():
-    func_with_defaults_that_will_change.__defaults__ = (4, 5)
-
-
-@cinder_support.failUnlessJITCompiled
-def func_that_change_defaults():
-    change_defaults()
-    return func_with_defaults_that_will_change()
-
-
 def compiled_code_func():
     pass
 
@@ -146,19 +87,6 @@ def compiled_code_func_with_nested():
         return 42
 
     return nested
-
-
-class InlinedFunctionTests(unittest.TestCase):
-    @jit_suppress
-    @passIf(
-        not cinderx.jit.is_hir_inliner_enabled(),
-        "meaningless without HIR inliner enabled",
-    )
-    def test_deopt_when_func_defaults_change(self) -> None:
-        self.assertEqual(
-            cinderx.jit.get_num_inlined_functions(func_that_change_defaults), 2
-        )
-        self.assertEqual(func_that_change_defaults(), 9)
 
 
 class InlineCacheStatsTests(unittest.TestCase):
@@ -226,81 +154,6 @@ class InlineCacheStatsTests(unittest.TestCase):
             binops_mul[1],
             {"count": 1, "reason": "Uncategorized"},
         )
-
-
-class InlinedFunctionLineNumberTests(unittest.TestCase):
-    @jit_suppress
-    @passIf(
-        not cinderx.jit.is_hir_inliner_enabled(),
-        "meaningless without HIR inliner enabled",
-    )
-    def test_line_numbers_with_sibling_inlined_functions(self) -> None:
-        """Verify that line numbers are correct when function calls are inlined in the same
-        expression"""
-        # Calls to get_stack and get_stack2 should be inlined
-        self.assertEqual(cinderx.jit.get_num_inlined_functions(get_stack_siblings), 2)
-        stacks = get_stack_siblings()
-        # Call to get_stack
-        self.assertEqual(stacks[0][-1].lineno, firstlineno(get_stack) + 3)
-        self.assertEqual(stacks[0][-2].lineno, firstlineno(get_stack_siblings) + 2)
-        # Call to get_stack2
-        self.assertEqual(stacks[1][-1].lineno, firstlineno(get_stack2) + 3)
-        self.assertEqual(stacks[1][-2].lineno, firstlineno(get_stack_siblings) + 2)
-
-    @jit_suppress
-    @passIf(
-        not cinderx.jit.is_hir_inliner_enabled(),
-        "meaningless without HIR inliner enabled",
-    )
-    def test_line_numbers_at_multiple_points_in_inlined_functions(self) -> None:
-        """Verify that line numbers are are correct at different points in an inlined
-        function"""
-        # Call to get_stack_multi should be inlined
-        self.assertEqual(cinderx.jit.get_num_inlined_functions(call_get_stack_multi), 1)
-        stacks = call_get_stack_multi()
-        self.assertEqual(stacks[0][-1].lineno, firstlineno(get_stack_multi) + 3)
-        self.assertEqual(stacks[0][-2].lineno, firstlineno(call_get_stack_multi) + 3)
-        self.assertEqual(stacks[1][-1].lineno, firstlineno(get_stack_multi) + 5)
-        self.assertEqual(stacks[1][-2].lineno, firstlineno(call_get_stack_multi) + 3)
-
-    @jit_suppress
-    @passIf(
-        not cinderx.jit.is_hir_inliner_enabled(),
-        "meaningless without HIR inliner enabled",
-    )
-    def test_inline_function_stats(self) -> None:
-        self.assertEqual(cinderx.jit.get_num_inlined_functions(func), 2)
-        stats = cinderx.jit.get_inlined_functions_stats(func)
-        self.assertEqual(stats.get("num_inlined_functions"), 2)
-        failure_stats = stats.get("failure_stats") or {}
-        assert isinstance(failure_stats, dict)
-        self.assertNotEqual(failure_stats, {})
-        has_varargs = failure_stats.get("HasVarargs") or set()
-        assert isinstance(has_varargs, set)
-        self.assertNotEqual(has_varargs, {})
-        self.assertEqual(len(has_varargs), 1, repr(has_varargs))
-        self.assertIn(
-            "test_cinderx.test_cinderjit:func_with_varargs", next(iter(has_varargs))
-        )
-
-    @jit_suppress
-    @passIf(
-        not cinderx.jit.is_hir_inliner_enabled(),
-        "meaningless without HIR inliner enabled",
-    )
-    def test_line_numbers_with_multiple_inlined_calls(self) -> None:
-        """Verify that line numbers are correct for inlined calls that appear
-        in different statements
-        """
-        # Call to get_stack should be inlined twice
-        self.assertEqual(cinderx.jit.get_num_inlined_functions(get_stack_twice), 2)
-        stacks = get_stack_twice()
-        # First call to double
-        self.assertEqual(stacks[0][-1].lineno, firstlineno(get_stack) + 3)
-        self.assertEqual(stacks[0][-2].lineno, firstlineno(get_stack_twice) + 3)
-        # Second call to double
-        self.assertEqual(stacks[1][-1].lineno, firstlineno(get_stack) + 3)
-        self.assertEqual(stacks[1][-2].lineno, firstlineno(get_stack_twice) + 4)
 
 
 class FaulthandlerTracebackTests(unittest.TestCase):
