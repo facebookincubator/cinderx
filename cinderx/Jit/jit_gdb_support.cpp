195f656f97445fcfb71d626c3fee7f524d800f96
Alex Malyshev <alexanderm@meta.com>
2026-05-26 20:13:50 -0700

    Make GDB support more C++ friendly
    Summary:
    Specifically getting ELFObjectContext to use a constructor and destructor.  The
    context can be constructed fully on the stack, there's no need to heap allocate
    it.
    
    Will follow this up with replacing jit_string_t with std::string.
    
    Reviewed By: DinoV
    
    Differential Revision: D106368522
    
    fbshipit-source-id: bea3542bf73a92a0e291d1a6774f8eeb575ed11c

--- cinderx/Jit/jit_gdb_support.cpp
+++ cinderx/Jit/jit_gdb_support.cpp
@@ -57,185 +57,7 @@ struct JITDescriptor {
 
 /* End GDB hook */
 
-// Forward declarations.
-struct ELFObjectContext;
-struct ELFObject;
-static ELFObjectContext* elfctx_new(
-    struct jit_string_t* filename,
-    int lineno,
-    struct jit_string_t* function_name,
-    void* code,
-    int code_size,
-    int stack_size);
-static void elfctx_free(ELFObjectContext* ctx);
-static void elfctx_build_object(ELFObjectContext* ctx);
-static ELFObject* elfctx_get_object_ptr(ELFObjectContext* ctx);
-static size_t elfctx_get_object_size(ELFObjectContext* ctx);
-
-static int
-register_elf_ctx(ELFObjectContext* ctx, const char* type, void* ptr) {
-  elfctx_build_object(ctx);
-
-  // Now we allocate enough space for both the JITCodeEntry and the
-  // ELFObject.
-  size_t elf_object_size = elfctx_get_object_size(ctx);
-  char* raw =
-      static_cast<char*>(calloc(1, sizeof(JITCodeEntry) + elf_object_size));
-  if (raw == nullptr) {
-    JIT_DLOG("Failed to allocate space for JITCodeEntry + ELFObject");
-    return 0;
-  }
-
-  char* elf_object_start = raw + sizeof(JITCodeEntry);
-
-  memcpy(elf_object_start, elfctx_get_object_ptr(ctx), elf_object_size);
-
-  if (jit::getConfig().gdb.write_elf_objects) {
-    // Write the ELF object to /tmp
-    struct jit_string_t* filename =
-        ss_sprintf_alloc("/tmp/cinder_%s_%p_elf", type, ptr);
-    int fd;
-    if ((fd = open(ss_get_string(filename), O_CREAT | O_RDWR, 0600))) {
-      if (write(fd, elf_object_start, elf_object_size) < 0) {
-        JIT_DLOG("Failed to write to {}", ss_get_string(filename));
-      }
-      close(fd);
-    }
-    ss_free(filename);
-  }
-
-  JITCodeEntry* entry = (JITCodeEntry*)raw;
-
-  entry->symfile_addr = elf_object_start;
-  entry->symfile_size = elf_object_size;
-
-  // Link into the list.
-  entry->prev_entry = nullptr;
-  JITCodeEntry* next_entry = __jit_debug_descriptor.first_entry;
-  entry->next_entry = next_entry;
-  if (next_entry) {
-    next_entry->prev_entry = entry;
-  }
-  __jit_debug_descriptor.first_entry = entry;
-  __jit_debug_descriptor.relevant_entry = entry;
-  __jit_debug_descriptor.action_flag = JIT_REGISTER_FN;
-
-  // Call the registration hook.
-  __jit_debug_register_code();
-
-  return 1;
-}
-
-int register_raw_debug_symbol(
-    const char* function_name,
-    const char* filename,
-    int lineno,
-    void* code_addr,
-    size_t code_size,
-    size_t stack_size) {
-  if (!jit::getConfig().gdb.supported) {
-    return 1;
-  }
-
-  ELFObjectContext* ctx = elfctx_new(
-      ss_sprintf_alloc("%s", filename),
-      lineno,
-      ss_sprintf_alloc("%s", function_name),
-      code_addr,
-      code_size,
-      stack_size);
-
-  if (ctx == nullptr) {
-    JIT_DLOG("Failed to allocate ELFObjectContext");
-    return 0;
-  }
-
-  if (!register_elf_ctx(ctx, function_name, code_addr)) {
-    elfctx_free(ctx);
-    return 0;
-  }
-
-  JIT_DLOG(
-      "Registered debug symbol at {} ({} bytes) for {} at {} ({} bytes)",
-      reinterpret_cast<void*>(elfctx_get_object_ptr(ctx)),
-      elfctx_get_object_size(ctx),
-      function_name,
-      code_addr,
-      code_size);
-
-  elfctx_free(ctx);
-
-  return 1;
-}
-
-int register_pycode_debug_symbol(
-    PyCodeObject* codeobj,
-    const char* fullname,
-    jit::CompiledFunctionData& compiled_func) {
-  if (!jit::getConfig().gdb.supported) {
-    return 1;
-  }
-
-  int code_size = compiled_func.code.size();
-  int stack_size = compiled_func.stack_size;
-  auto code =
-      const_cast<void*>(reinterpret_cast<const void*>(&compiled_func.code[0]));
-  if (code_size < 1) {
-    JIT_DLOG(
-        "Not registering symbol at {} because it has an invalid size {}",
-        code,
-        code_size);
-    return 0;
-  }
-  if (stack_size == -1) {
-    JIT_DLOG(
-        "Not registering symbol at {} because it has an invalid "
-        "stack size {}",
-        code,
-        stack_size);
-    return 0;
-  }
-
-  PyObject* sourcefile = codeobj->co_filename;
-  const char* filename = "<unknown>";
-  if (sourcefile != nullptr && PyUnicode_Check(sourcefile)) {
-    filename = PyUnicode_AsUTF8(sourcefile);
-    if (filename == nullptr) {
-      filename = "<filename failed to encode to UTF8>";
-      JIT_DLOG("Failed to encode filename for ELFObjectContext");
-    }
-  }
-
-  ELFObjectContext* ctx = elfctx_new(
-      ss_sprintf_alloc("%s", filename),
-      codeobj->co_firstlineno,
-      ss_sprintf_alloc("%s", fullname),
-      code,
-      code_size,
-      stack_size);
-
-  if (ctx == nullptr) {
-    JIT_DLOG("Failed to allocate ELFObjectContext");
-    return 0;
-  }
-
-  if (!register_elf_ctx(ctx, "PyFunctionObject", code)) {
-    elfctx_free(ctx);
-    return 0;
-  }
-
-  JIT_DLOG(
-      "Registered debug symbol at {} ({} bytes) for a function at {} "
-      "({} bytes)",
-      reinterpret_cast<void*>(elfctx_get_object_ptr(ctx)),
-      elfctx_get_object_size(ctx),
-      code,
-      code_size);
-
-  elfctx_free(ctx);
-
-  return 1;
-}
+namespace {
 
 /* In-memory ELF object generation -- largely borrowed from LuaJIT's
  * implementation. There is much magic here... I've tried to rationalize it a
@@ -383,14 +205,41 @@ enum { ELF_SYM_UNDEF, ELF_SYM_FILE, ELF_SYM_FUNC, ELF_SYM__MAX };
 
 /* In-memory ELF object. */
 struct ELFObject {
+  ELFObject() {
+    std::memset(this, 0, sizeof(*this));
+  }
+
   ELFHeader hdr; /* ELF header. */
   ELFSectionHeader sect[ELF_SECT__MAX]; /* ELF sections. */
   ELFSymbol sym[ELF_SYM__MAX]; /* ELF symbol table. */
   uint8_t space[4096]; /* Space for various section data. */
 };
 
+/* Context for generating the ELF object for the GDB JIT API. */
+struct ELFObjectContext {
+  ~ELFObjectContext() {
+    if (filename) {
+      ss_free(filename);
+    }
+    if (function_name) {
+      ss_free(function_name);
+    }
+  }
+
+  uint8_t* p{nullptr}; /* Pointer to next address in obj.space. */
+  uint8_t* startp{nullptr}; /* Pointer to start address in obj.space. */
+  struct jit_string_t* function_name{nullptr}; /* The python function name */
+  struct jit_string_t* filename{nullptr}; /* File the function came from */
+  int lineno{0}; /* The first line of the python definition */
+  uintptr_t code_addr{0}; /* Machine code address. */
+  uint32_t code_size{0}; /* Size of machine code. */
+  uint32_t stack_size{0}; /* Stack adjustment for trace itself. */
+  size_t objsize{0}; /* Final size of ELF object. */
+  ELFObject obj; /* In-memory ELF object. */
+};
+
 /* Template for in-memory ELF header. */
-static const ELFHeader elfhdr_template = {
+const ELFHeader elfhdr_template = {
     .emagic = {0x7f, 'E', 'L', 'F'},
     .eclass = 2,
     .eendian = 1,
@@ -412,22 +261,64 @@ static const ELFHeader elfhdr_template = {
     .shnum = ELF_SECT__MAX,
     .shstridx = ELF_SECT_shstrtab};
 
-/* Context for generating the ELF object for the GDB JIT API. */
-struct ELFObjectContext {
-  uint8_t* p; /* Pointer to next address in obj.space. */
-  uint8_t* startp; /* Pointer to start address in obj.space. */
-  struct jit_string_t* function_name; /* The python function name */
-  struct jit_string_t* filename; /* The python file the function came from */
-  int lineno; /* The first line of the python definition */
-  uintptr_t code_addr; /* Machine code address. */
-  uint32_t code_size; /* Size of machine code. */
-  uint32_t stack_size; /* Stack adjustment for trace itself. */
-  size_t objsize; /* Final size of ELF object. */
-  ELFObject obj; /* In-memory ELF object. */
-};
+// Forward declarations.
+void elfctx_build_object(ELFObjectContext* ctx);
+
+int register_elf_ctx(ELFObjectContext* ctx, const char* type, void* ptr) {
+  elfctx_build_object(ctx);
+
+  // Now we allocate enough space for both the JITCodeEntry and the
+  // ELFObject.
+  size_t elf_object_size = ctx->objsize;
+  char* raw =
+      static_cast<char*>(calloc(1, sizeof(JITCodeEntry) + elf_object_size));
+  if (raw == nullptr) {
+    JIT_DLOG("Failed to allocate space for JITCodeEntry + ELFObject");
+    return 0;
+  }
+
+  char* elf_object_start = raw + sizeof(JITCodeEntry);
+
+  memcpy(elf_object_start, &ctx->obj, elf_object_size);
+
+  if (jit::getConfig().gdb.write_elf_objects) {
+    // Write the ELF object to /tmp
+    struct jit_string_t* filename =
+        ss_sprintf_alloc("/tmp/cinder_%s_%p_elf", type, ptr);
+    int fd;
+    if ((fd = open(ss_get_string(filename), O_CREAT | O_RDWR, 0600))) {
+      if (write(fd, elf_object_start, elf_object_size) < 0) {
+        JIT_DLOG("Failed to write to {}", ss_get_string(filename));
+      }
+      close(fd);
+    }
+    ss_free(filename);
+  }
+
+  JITCodeEntry* entry = (JITCodeEntry*)raw;
+
+  entry->symfile_addr = elf_object_start;
+  entry->symfile_size = elf_object_size;
+
+  // Link into the list.
+  entry->prev_entry = nullptr;
+  JITCodeEntry* next_entry = __jit_debug_descriptor.first_entry;
+  entry->next_entry = next_entry;
+  if (next_entry) {
+    next_entry->prev_entry = entry;
+  }
+  __jit_debug_descriptor.first_entry = entry;
+  __jit_debug_descriptor.relevant_entry = entry;
+  __jit_debug_descriptor.action_flag = JIT_REGISTER_FN;
+
+  // Call the registration hook.
+  __jit_debug_register_code();
+
+  return 1;
+}
 
 /* Append a null-terminated string. */
-static uint32_t elfctx_append_string(ELFObjectContext* ctx, const char* str) {
+uint32_t elfctx_append_string(ELFObjectContext* ctx, const char* str) {
   uint8_t* p = ctx->p;
   uint32_t ofs = (uint32_t)(p - ctx->startp);
   do {
@@ -438,7 +329,7 @@ static uint32_t elfctx_append_string(ELFObjectContext* ctx, const char* str) {
 }
 
 /* Append a SLEB128 value. */
-static void elfctx_append_sleb128(ELFObjectContext* ctx, int32_t v) {
+void elfctx_append_sleb128(ELFObjectContext* ctx, int32_t v) {
   uint8_t* p = ctx->p;
   for (; (uint32_t)(v + 0x40) >= 0x80; v >>= 7) {
     *p++ = (uint8_t)((v & 0x7f) | 0x80);
@@ -448,7 +339,7 @@ static void elfctx_append_sleb128(ELFObjectContext* ctx, int32_t v) {
 }
 
 /* Append a ULEB128 to buffer. */
-static void elfctx_append_uleb128(ELFObjectContext* ctx, uint32_t v) {
+void elfctx_append_uleb128(ELFObjectContext* ctx, uint32_t v) {
   uint8_t* p = ctx->p;
   for (; v >= 0x80; v >>= 7) {
     *p++ = (char)((v & 0x7f) | 0x80);
@@ -479,7 +370,7 @@ static void elfctx_append_uleb128(ELFObjectContext* ctx, uint32_t v) {
   }
 
 /* Initialize ELF section headers. */
-static void elf_secthdr(ELFObjectContext* ctx) {
+void elf_secthdr(ELFObjectContext* ctx) {
   ELFSectionHeader* sect;
 
   *ctx->p++ = '\0'; /* Empty string at start of string table. */
@@ -517,7 +408,7 @@ static void elf_secthdr(ELFObjectContext* ctx) {
 }
 
 /* Initialize symbol table. */
-static void elf_init_symtab(ELFObjectContext* ctx) {
+void elf_init_symtab(ELFObjectContext* ctx) {
   ELFSymbol* sym;
 
   *ctx->p++ = '\0'; /* Empty string at start of string table. */
@@ -539,7 +430,7 @@ static void elf_init_symtab(ELFObjectContext* ctx) {
 }
 
 /* Initialize .debug_info section. */
-static void elf_init_debuginfo(ELFObjectContext* ctx) {
+void elf_init_debuginfo(ELFObjectContext* ctx) {
   uint8_t* p = ctx->p;
 
   DWRF_SECTION(
@@ -560,7 +451,7 @@ static void elf_init_debuginfo(ELFObjectContext* ctx) {
 }
 
 /* Initialize .debug_abbrev section. */
-static void elf_init_debugabbrev(ELFObjectContext* ctx) {
+void elf_init_debugabbrev(ELFObjectContext* ctx) {
   uint8_t* p = ctx->p;
 
   /* Abbrev #1: DWRF_TAG_compile_unit. */
@@ -586,7 +477,7 @@ static void elf_init_debugabbrev(ELFObjectContext* ctx) {
   (DWRF_U8(DWRF_LNS_extended_op), DWRF_UV(1 + (s)), DWRF_U8((op)))
 
 /* Initialize .debug_line section. */
-static void elf_init_debugline(ELFObjectContext* ctx) {
+void elf_init_debugline(ELFObjectContext* ctx) {
   uint8_t* p = ctx->p;
 
   DWRF_SECTION(
@@ -643,8 +534,7 @@ static void elf_init_debugline(ELFObjectContext* ctx) {
 using ELFSectionInitFn = void (*)(ELFObjectContext* ctx);
 
 /* Call section initializer and set the section offset and size. */
-static void
-elf_init_section(ELFObjectContext* ctx, int sect, ELFSectionInitFn init) {
+void elf_init_section(ELFObjectContext* ctx, int sect, ELFSectionInitFn init) {
   ctx->startp = ctx->p;
   ctx->obj.sect[sect].ofs = (uintptr_t)((char*)ctx->p - (char*)&ctx->obj);
   init(ctx);
@@ -655,7 +545,7 @@ elf_init_section(ELFObjectContext* ctx, int sect, ELFSectionInitFn init) {
   ((p) = (uint8_t*)(((uintptr_t)(p) + ((a) - 1)) & ~(uintptr_t)((a) - 1)))
 
 /* Build in-memory ELF object. */
-static void elfctx_build_object(ELFObjectContext* ctx) {
+void elfctx_build_object(ELFObjectContext* ctx) {
   ELFObject* obj = &ctx->obj;
   /* Fill in ELF header and clear structures. */
   memcpy(&obj->hdr, &elfhdr_template, sizeof(ELFHeader));
@@ -678,41 +568,99 @@ static void elfctx_build_object(ELFObjectContext* ctx) {
 
 #undef ALIGN_SECTION
 
-static ELFObjectContext* elfctx_new(
-    struct jit_string_t* filename,
+} // namespace
+
+int register_raw_debug_symbol(
+    const char* function_name,
+    const char* filename,
     int lineno,
-    struct jit_string_t* function_name,
-    void* code,
-    int code_size,
-    int stack_size) {
-  JIT_DCHECK(code_size >= 0, "code_size must be greater than zero");
-  JIT_DCHECK(stack_size >= 0, "stack_size must be greater than zero");
-  ELFObjectContext* ctx =
-      static_cast<ELFObjectContext*>(calloc(1, sizeof(ELFObjectContext)));
-  if (ctx == nullptr) {
-    return nullptr;
+    void* code_addr,
+    size_t code_size,
+    size_t stack_size) {
+  if (!jit::getConfig().gdb.supported) {
+    return 1;
   }
 
-  ctx->code_addr = (uintptr_t)code;
-  ctx->code_size = (uint32_t)code_size;
-  ctx->stack_size = (uint32_t)stack_size;
-  ctx->filename = filename;
-  ctx->lineno = lineno;
-  ctx->function_name = function_name;
+  ELFObjectContext ctx;
+  ctx.code_addr = reinterpret_cast<uintptr_t>(code_addr);
+  ctx.code_size = static_cast<uint32_t>(code_size);
+  ctx.stack_size = static_cast<uint32_t>(stack_size);
+  ctx.filename = ss_sprintf_alloc("%s", filename);
+  ctx.lineno = lineno;
+  ctx.function_name = ss_sprintf_alloc("%s", function_name);
 
-  return ctx;
+  if (!register_elf_ctx(&ctx, function_name, code_addr)) {
+    return 0;
+  }
+
+  JIT_DLOG(
+      "Registered debug symbol at {} ({} bytes) for {} at {} ({} bytes)",
+      reinterpret_cast<void*>(&ctx.obj),
+      ctx.objsize,
+      function_name,
+      code_addr,
+      code_size);
+
+  return 1;
 }
 
-static void elfctx_free(ELFObjectContext* ctx) {
-  ss_free(ctx->filename);
-  ss_free(ctx->function_name);
-  free(ctx);
-}
+int register_pycode_debug_symbol(
+    PyCodeObject* codeobj,
+    const char* fullname,
+    jit::CompiledFunctionData& compiled_func) {
+  if (!jit::getConfig().gdb.supported) {
+    return 1;
+  }
 
-static ELFObject* elfctx_get_object_ptr(ELFObjectContext* ctx) {
-  return &ctx->obj;
-}
+  size_t code_size = compiled_func.code.size();
+  int stack_size = compiled_func.stack_size;
+  auto code =
+      const_cast<void*>(reinterpret_cast<const void*>(&compiled_func.code[0]));
+  if (code_size < 1) {
+    JIT_DLOG(
+        "Not registering symbol at {} because it has an invalid size {}",
+        code,
+        code_size);
+    return 0;
+  }
+  if (stack_size == -1) {
+    JIT_DLOG(
+        "Not registering symbol at {} because it has an invalid "
+        "stack size {}",
+        code,
+        stack_size);
+    return 0;
+  }
 
-static size_t elfctx_get_object_size(ELFObjectContext* ctx) {
-  return ctx->objsize;
+  PyObject* sourcefile = codeobj->co_filename;
+  const char* filename = "<unknown>";
+  if (sourcefile != nullptr && PyUnicode_Check(sourcefile)) {
+    filename = PyUnicode_AsUTF8(sourcefile);
+    if (filename == nullptr) {
+      filename = "<filename failed to encode to UTF8>";
+      JIT_DLOG("Failed to encode filename for ELFObjectContext");
+    }
+  }
+
+  ELFObjectContext ctx;
+  ctx.code_addr = reinterpret_cast<uintptr_t>(code);
+  ctx.code_size = static_cast<uint32_t>(code_size);
+  ctx.stack_size = static_cast<uint32_t>(stack_size);
+  ctx.filename = ss_sprintf_alloc("%s", filename);
+  ctx.lineno = codeobj->co_firstlineno;
+  ctx.function_name = ss_sprintf_alloc("%s", fullname);
+
+  if (!register_elf_ctx(&ctx, "PyFunctionObject", code)) {
+    return 0;
+  }
+
+  JIT_DLOG(
+      "Registered debug symbol at {} ({} bytes) for a function at {} "
+      "({} bytes)",
+      reinterpret_cast<void*>(&ctx.obj),
+      ctx.objsize,
+      code,
+      code_size);
+
+  return 1;
 }
