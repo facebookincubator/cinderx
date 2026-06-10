dc2ea12e8d45ee896b8cbf5806a84976a59b9529
Kevin Newton <kddnewton@meta.com>
2026-06-10 15:14:15 -0700

    aarch64 comparecc optimizations
    Summary: Add a pattern match for aarch64 over comparisons feeding directly into a branchcc. In this case the comparison would normally emit a compare and then a cset, but we can skip the cset entirely and do a conditional branch instead. Notably this pattern appears in some of the microbenchmarks like richards.
    
    Reviewed By: DinoV
    
    Differential Revision: D107693088
    
    fbshipit-source-id: c7eae9f43c565507408b04ea278f4976f0f41fb8

--- cinderx/Jit/lir/target_select.cpp
+++ cinderx/Jit/lir/target_select.cpp
@@ -3,6 +3,14 @@
 #include "cinderx/Jit/lir/target_select.h"
 
 #include "cinderx/Jit/codegen/arch/detection.h"
+#include "cinderx/Jit/lir/block.h"
+#include "cinderx/Jit/lir/function.h"
+#include "cinderx/Jit/lir/instruction.h"
+#include "cinderx/Jit/lir/operand.h"
+
+#include <iterator>
+#include <memory>
+#include <unordered_map>
 
 namespace jit::lir {
 namespace {
@@ -12,8 +20,124 @@ void selectX64Opcodes(Function* func) {
   (void)func;
 }
 #elif defined(CINDER_AARCH64)
+using UseCounts = std::unordered_map<const Instruction*, size_t>;
+
+void countOperandUse(UseCounts& use_counts, const Operand* operand) {
+  if (operand->isLinked()) {
+    use_counts[operand->getLinkedInstr()]++;
+    return;
+  }
+
+  if (!operand->isInd()) {
+    return;
+  }
+
+  MemoryIndirect* indirect = operand->getMemoryIndirect();
+  Operand* base = indirect->getBaseRegOperand();
+  if (base->isLinked()) {
+    use_counts[base->getLinkedInstr()]++;
+  }
+
+  Operand* index = indirect->getIndexRegOperand();
+  if (index != nullptr && index->isLinked()) {
+    use_counts[index->getLinkedInstr()]++;
+  }
+}
+
+/* Count the number of uses of each instruction in the function. This
+ * information should really be stored in the IR so that we have either use
+ * counts or use-def chains, but for now in order to get this functional we're
+ * going to count the uses here.
+ */
+UseCounts countUses(Function* func) {
+  UseCounts use_counts;
+  for (BasicBlock* block : func->basicblocks()) {
+    for (std::unique_ptr<Instruction>& instr : block->instructions()) {
+      instr->foreachInputOperand([&use_counts](const Operand* operand) {
+        countOperandUse(use_counts, operand);
+      });
+    }
+  }
+  return use_counts;
+}
+
+/* Check that intervening instructions between two iterator points do not modify
+ * flags in any way. This allows the two endpoints to reliably set/get flags. */
+bool flagsPreservedBetween(instr_iter_t begin, instr_iter_t end) {
+  for (instr_iter_t iter = begin; iter != end; iter++) {
+    if (InstrProperty::getProperties(iter->get()->opcode()).flag_effects !=
+        FlagEffects::kNone) {
+      return false;
+    }
+  }
+  return true;
+}
+
+/* Convert from:
+ *
+ *     cmp x0, x1
+ *     cset w2, eq
+ *     b.eq label
+ *
+ * to:
+ *
+ *     cmp x0, x1
+ *     b.eq label
+ */
+void selectA64CondBranch(
+    BasicBlock* block,
+    instr_iter_t instr_iter,
+    const UseCounts& use_counts) {
+  Instruction* branch = instr_iter->get();
+  JIT_DCHECK(
+      branch->isCondBranch(), "Expected CondBranch, got {}", branch->opname());
+
+  /* Check that the input to this conditional branch is not a def. */
+  Operand* input = branch->getInput(0);
+  if (!input->isLinked()) {
+    return;
+  }
+
+  /* Check that the input to this conditional branch is a compare in the same
+   * block and that it is not used by any other instruction. */
+  Instruction* compare = input->getLinkedInstr();
+  if (!compare->isCompare() || compare->basicblock() != block ||
+      use_counts.at(compare) != 1) {
+    return;
+  }
+
+  /* Check that the instructions between the compare and the conditional branch
+   * do not modify flags. */
+  instr_iter_t compare_iter = block->iterator_to(compare);
+  if (!flagsPreservedBetween(std::next(compare_iter), instr_iter)) {
+    return;
+  }
+
+  /* Convert to a conditional compare and branch instruction. */
+  Instruction::Opcode branch_opcode =
+      Instruction::compareToBranchCC(compare->opcode());
+
+  compare->setOpcode(Instruction::kCmp);
+  compare->output()->setNone();
+  branch->setOpcode(branch_opcode);
+  branch->setNumInputs(0);
+}
+
 void selectA64Opcodes(Function* func) {
-  (void)func;
+  UseCounts use_counts = countUses(func);
+  for (BasicBlock* block : func->basicblocks()) {
+    BasicBlock::InstrList& instrs = block->instructions();
+    for (instr_iter_t iter = instrs.begin(); iter != instrs.end();) {
+      instr_iter_t cur_iter = iter++;
+      switch (cur_iter->get()->opcode()) {
+        case Instruction::kCondBranch:
+          selectA64CondBranch(block, cur_iter, use_counts);
+          break;
+        default:
+          break;
+      }
+    }
+  }
 }
 #else
 void selectUnknownTargetOpcodes(Function* func) {
---
+++ cinderx/RuntimeTests/lir_target_select_test.cpp
@@ -0,0 +1,181 @@
+// Copyright (c) Meta Platforms, Inc. and affiliates.
+
+#include <gtest/gtest.h>
+
+#include "cinderx/Jit/code_runtime.h"
+#include "cinderx/Jit/codegen/environ.h"
+#include "cinderx/Jit/compiler.h"
+#include "cinderx/Jit/context.h"
+#include "cinderx/Jit/hir/hir.h"
+#include "cinderx/Jit/lir/generator.h"
+#include "cinderx/Jit/lir/parser.h"
+#include "cinderx/Jit/lir/target_select.h"
+#include "cinderx/RuntimeTests/fixtures.h"
+
+#include <memory>
+#include <sstream>
+#include <string>
+
+namespace jit::lir {
+
+class LIRTargetSelectTest : public RuntimeTest {
+ public:
+  std::string getSelectedLIRString(PyObject* func_obj) {
+    JIT_CHECK(
+        PyFunction_Check(func_obj),
+        "Trying to compile something that isn't a function");
+    BorrowedRef<PyFunctionObject> func{func_obj};
+
+    PyObject* globals = PyFunction_GetGlobals(func_obj);
+    if (!PyDict_CheckExact(globals) ||
+        !PyDict_CheckExact(func->func_builtins)) {
+      return "";
+    }
+
+    std::unique_ptr<hir::Function> irfunc(buildHIR(func));
+    Compiler::runPasses(*irfunc, PassConfig::kAllExceptInliner);
+
+    codegen::Environ env;
+    env.ctx = getContext();
+
+    CodeRuntime runtime{func};
+    runtime.setReifier(irfunc->reifier);
+    env.code_rt = &runtime;
+
+    LIRGenerator lir_gen(irfunc.get(), &env);
+    std::unique_ptr<Function> lir_func = lir_gen.TranslateFunction();
+    selectTargetOpcodes(lir_func.get());
+
+    std::stringstream ss;
+    lir_func->sortBasicBlocks();
+    ss << *lir_func << '\n';
+    return ss.str();
+  }
+};
+
+#if defined(CINDER_AARCH64)
+static std::string runTargetSelect(const char* lir_input_str) {
+  std::unique_ptr<Function> func = Parser().parse(lir_input_str);
+  selectTargetOpcodes(func.get());
+  return fmt::format("{}", *func);
+}
+
+TEST_F(LIRTargetSelectTest, SelectsBranchCCForSingleUseCompare) {
+  const char* lir_input_str = R"(Function:
+BB %0 - succs: %1 %2
+  %1:64bit = Move 1
+  %2:64bit = Move 2
+  %3:8bit = Equal %1, %2
+  CondBranch %3, BB%1, BB%2
+BB %1 - preds: %0
+  Return %1
+BB %2 - preds: %0
+  Return %2
+)";
+
+  const char* expected_lir_str = R"(Function:
+BB %0 - succs: %1 %2
+        %1:64bit = Move 1(0x1):64bit
+        %2:64bit = Move 2(0x2):64bit
+                   Cmp %1:64bit, %2:64bit
+                   BranchE
+
+BB %1 - preds: %0
+                   Return %1:64bit
+
+BB %2 - preds: %0
+                   Return %2:64bit
+
+)";
+
+  EXPECT_EQ(runTargetSelect(lir_input_str), expected_lir_str);
+}
+
+TEST_F(
+    LIRTargetSelectTest,
+    SelectsBranchCCWhenInterveningInstructionsPreserveFlags) {
+  const char* lir_input_str = R"(Function:
+BB %0 - succs: %1 %2
+  %1:64bit = Move 1
+  %2:64bit = Move 2
+  %3:8bit = Equal %1, %2
+  %4:64bit = Move 3
+  CondBranch %3, BB%1, BB%2
+BB %1 - preds: %0
+  Return %1
+BB %2 - preds: %0
+  Return %2
+)";
+
+  const char* expected_lir_str = R"(Function:
+BB %0 - succs: %1 %2
+        %1:64bit = Move 1(0x1):64bit
+        %2:64bit = Move 2(0x2):64bit
+                   Cmp %1:64bit, %2:64bit
+        %4:64bit = Move 3(0x3):64bit
+                   BranchE
+
+BB %1 - preds: %0
+                   Return %1:64bit
+
+BB %2 - preds: %0
+                   Return %2:64bit
+
+)";
+
+  EXPECT_EQ(runTargetSelect(lir_input_str), expected_lir_str);
+}
+
+TEST_F(LIRTargetSelectTest, DoesNotSelectBranchCCAcrossFlagClobber) {
+  const char* lir_input_str = R"(Function:
+BB %0 - succs: %1 %2
+  %1:64bit = Move 1
+  %2:64bit = Move 2
+  %3:8bit = Equal %1, %2
+  %4:64bit = Add %1, %2
+  CondBranch %3, BB%1, BB%2
+BB %1 - preds: %0
+  Return %1
+BB %2 - preds: %0
+  Return %2
+)";
+
+  const char* expected_lir_str = R"(Function:
+BB %0 - succs: %1 %2
+        %1:64bit = Move 1(0x1):64bit
+        %2:64bit = Move 2(0x2):64bit
+         %3:8bit = Equal %1:64bit, %2:64bit
+        %4:64bit = Add %1:64bit, %2:64bit
+                   CondBranch %3:8bit, BB%1, BB%2
+
+BB %1 - preds: %0
+                   Return %1:64bit
+
+BB %2 - preds: %0
+                   Return %2:64bit
+
+)";
+
+  EXPECT_EQ(runTargetSelect(lir_input_str), expected_lir_str);
+}
+
+TEST_F(LIRTargetSelectTest, SelectsBranchCCForPythonCompareBranch) {
+  const char* src = R"(
+def func(x, y):
+  if x in y:
+    return x
+  return y
+)";
+
+  Ref<PyObject> pyfunc(compileAndGet(src, "func"));
+  ASSERT_NE(pyfunc.get(), nullptr) << "Failed compiling func";
+
+  std::string lir_str = getSelectedLIRString(pyfunc.get());
+
+  EXPECT_NE(lir_str.find("Cmp "), std::string::npos) << lir_str;
+  EXPECT_NE(lir_str.find("BranchE"), std::string::npos);
+  EXPECT_EQ(lir_str.find(" = Equal "), std::string::npos) << lir_str;
+}
+#endif
+
+} // namespace jit::lir
