dcced3ef39c0c387ae00281ff775cca7a2674118
generatedunixname1062969949410264 <noreply+1062969949410264@fb.com>
2026-08-06 07:59:10 -0700

    arvr/tools/build_defs/config/toolchain/python/constraints/BUCK
    Reviewed By: alexmalyshev
    
    Differential Revision: D115015093
    
    fbshipit-source-id: 13bde4d9e2c4eabc8001213da87f8fa14ada2805

--- cinderx/Interpreter/regen-opcodes-312.sh
+++ cinderx/Interpreter/regen-opcodes-312.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 # (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
 
-OUT=$(buck build -m ovr_config//toolchain/python/constraints:3.12 fbcode//cinderx/Interpreter:gen_opcode_h --show-full-simple-output)
+OUT=$(buck build -m 'ovr_config//toolchain/python/constraints:python-version[3.12]' fbcode//cinderx/Interpreter:gen_opcode_h --show-full-simple-output)
 cp "$OUT" 3.12/opcode.h
 
-OUT=$(buck build -m ovr_config//toolchain/python/constraints:3.12 fbcode//cinderx/Interpreter:gen_opcode_h\[cinder_opcode.h\] --show-full-simple-output)
+OUT=$(buck build -m 'ovr_config//toolchain/python/constraints:python-version[3.12]' fbcode//cinderx/Interpreter:gen_opcode_h\[cinder_opcode.h\] --show-full-simple-output)
 cp "$OUT" 3.12/cinder_opcode_metadata.h
 
-OUT=$(buck build -m ovr_config//toolchain/python/constraints:3.12 fbcode//cinderx/Interpreter:gen_cinderx_opcode_targets_h --show-full-simple-output)
+OUT=$(buck build -m 'ovr_config//toolchain/python/constraints:python-version[3.12]' fbcode//cinderx/Interpreter:gen_cinderx_opcode_targets_h --show-full-simple-output)
 cp "$OUT" 3.12/cinderx_opcode_targets.h
