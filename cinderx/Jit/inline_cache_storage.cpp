0c70909da655b1363e03cb611719e41bf7f7eabb
Kevin Newton <kddnewton@meta.com>
2026-08-11 12:34:28 -0700

    Add per-compilation inline cache storage
    Summary: Add an arena-backed inline cache storage implementation for one generated-code version and record cache sites by kind and bytecode offset for future feedback-vector construction. Continue selecting ContextInlineCacheStorage in this diff, so cache ownership and runtime behavior remain unchanged until the final wiring diff.
    
    Reviewed By: mpage, DinoV
    
    Differential Revision: D114255370
    
    fbshipit-source-id: 36d70ed97246952c9d36aa46ad69b6aef19dfec6

--- cinderx/Jit/inline_cache_storage.cpp
+++ cinderx/Jit/inline_cache_storage.cpp
@@ -2,7 +2,159 @@
 
 #include "cinderx/Jit/inline_cache_storage.h"
 
+#include <utility>
+
 namespace cinderx::jit {
+
+InlineCacheSite::InlineCacheSite(
+    BCOffset bytecode_offset,
+    LoadAttrCache* cache_ptr)
+    : kind{Kind::kLoadAttr}, bytecode_offset{bytecode_offset} {
+  cache.load_attr = cache_ptr;
+}
+
+InlineCacheSite::InlineCacheSite(
+    BCOffset bytecode_offset,
+    LoadMethodCache* cache_ptr)
+    : kind{Kind::kLoadMethod}, bytecode_offset{bytecode_offset} {
+  cache.load_method = cache_ptr;
+}
+
+InlineCacheSite::InlineCacheSite(
+    BCOffset bytecode_offset,
+    LoadModuleAttrCache* cache_ptr)
+    : kind{Kind::kLoadModuleAttr}, bytecode_offset{bytecode_offset} {
+  cache.load_module_attr = cache_ptr;
+}
+
+InlineCacheSite::InlineCacheSite(
+    BCOffset bytecode_offset,
+    LoadModuleMethodCache* cache_ptr)
+    : kind{Kind::kLoadModuleMethod}, bytecode_offset{bytecode_offset} {
+  cache.load_module_method = cache_ptr;
+}
+
+InlineCacheSite::InlineCacheSite(
+    BCOffset bytecode_offset,
+    LoadTypeAttrCache* cache_ptr)
+    : kind{Kind::kLoadTypeAttr}, bytecode_offset{bytecode_offset} {
+  cache.load_type_attr = cache_ptr;
+}
+
+InlineCacheSite::InlineCacheSite(
+    BCOffset bytecode_offset,
+    LoadTypeMethodCache* cache_ptr)
+    : kind{Kind::kLoadTypeMethod}, bytecode_offset{bytecode_offset} {
+  cache.load_type_method = cache_ptr;
+}
+
+InlineCacheSite::InlineCacheSite(
+    BCOffset bytecode_offset,
+    BinaryOpCache* cache_ptr)
+    : kind{Kind::kBinaryOp}, bytecode_offset{bytecode_offset} {
+  cache.binary_op = cache_ptr;
+}
+
+InlineCacheSite::InlineCacheSite(
+    BCOffset bytecode_offset,
+    StoreAttrCache* cache_ptr)
+    : kind{Kind::kStoreAttr}, bytecode_offset{bytecode_offset} {
+  cache.store_attr = cache_ptr;
+}
+
+const std::vector<InlineCacheSite>&
+PerCompilationInlineCacheStorage::inlineCacheSites() const {
+  return inline_cache_sites_;
+}
+
+LoadAttrCache* PerCompilationInlineCacheStorage::allocateLoadAttrCache(
+    BCOffset bytecode_offset) {
+  auto cache =
+      inline_cache_arena_.allocate<LoadAttrCache, AttributeCacheSizeTrait>();
+  addInlineCacheSite(InlineCacheSite{bytecode_offset, cache});
+  return cache;
+}
+
+LoadMethodCache* PerCompilationInlineCacheStorage::allocateLoadMethodCache(
+    BCOffset bytecode_offset) {
+  auto cache = inline_cache_arena_.allocate<LoadMethodCache>();
+  addInlineCacheSite(InlineCacheSite{bytecode_offset, cache});
+  return cache;
+}
+
+LoadModuleAttrCache*
+PerCompilationInlineCacheStorage::allocateLoadModuleAttrCache(
+    BCOffset bytecode_offset) {
+  auto cache = inline_cache_arena_.allocate<LoadModuleAttrCache>();
+  addInlineCacheSite(InlineCacheSite{bytecode_offset, cache});
+  return cache;
+}
+
+LoadModuleMethodCache*
+PerCompilationInlineCacheStorage::allocateLoadModuleMethodCache(
+    BCOffset bytecode_offset) {
+  auto cache = inline_cache_arena_.allocate<LoadModuleMethodCache>();
+  addInlineCacheSite(InlineCacheSite{bytecode_offset, cache});
+  return cache;
+}
+
+LoadTypeAttrCache* PerCompilationInlineCacheStorage::allocateLoadTypeAttrCache(
+    BCOffset bytecode_offset) {
+  auto cache = allocateLoadTypeAttrCache();
+  addLoadTypeAttrCacheSite(bytecode_offset, cache);
+  return cache;
+}
+
+LoadTypeAttrCache*
+PerCompilationInlineCacheStorage::allocateLoadTypeAttrCache() {
+  return inline_cache_arena_.allocate<LoadTypeAttrCache>();
+}
+
+LoadTypeMethodCache*
+PerCompilationInlineCacheStorage::allocateLoadTypeMethodCache(
+    BCOffset bytecode_offset) {
+  auto cache = allocateLoadTypeMethodCache();
+  addLoadTypeMethodCacheSite(bytecode_offset, cache);
+  return cache;
+}
+
+LoadTypeMethodCache*
+PerCompilationInlineCacheStorage::allocateLoadTypeMethodCache() {
+  return inline_cache_arena_.allocate<LoadTypeMethodCache>();
+}
+
+BinaryOpCache* PerCompilationInlineCacheStorage::allocateBinaryOpCache(
+    BCOffset bytecode_offset,
+    hir::BinaryOpKind op) {
+  auto cache = inline_cache_arena_.allocate<BinaryOpCache>(op);
+  addInlineCacheSite(InlineCacheSite{bytecode_offset, cache});
+  return cache;
+}
+
+StoreAttrCache* PerCompilationInlineCacheStorage::allocateStoreAttrCache(
+    BCOffset bytecode_offset) {
+  auto cache =
+      inline_cache_arena_.allocate<StoreAttrCache, AttributeCacheSizeTrait>();
+  addInlineCacheSite(InlineCacheSite{bytecode_offset, cache});
+  return cache;
+}
+
+void PerCompilationInlineCacheStorage::addLoadTypeAttrCacheSite(
+    BCOffset bytecode_offset,
+    LoadTypeAttrCache* cache) {
+  addInlineCacheSite(InlineCacheSite{bytecode_offset, cache});
+}
+
+void PerCompilationInlineCacheStorage::addLoadTypeMethodCacheSite(
+    BCOffset bytecode_offset,
+    LoadTypeMethodCache* cache) {
+  addInlineCacheSite(InlineCacheSite{bytecode_offset, cache});
+}
+
+void PerCompilationInlineCacheStorage::addInlineCacheSite(
+    InlineCacheSite site) {
+  inline_cache_sites_.emplace_back(std::move(site));
+}
 
 LoadAttrCache* ContextInlineCacheStorage::allocateLoadAttrCache(
     [[maybe_unused]] BCOffset bytecode_offset) {
@@ -40,6 +192,12 @@ LoadTypeMethodCache* ContextInlineCacheStorage::allocateLoadTypeMethodCache() {
 LoadTypeMethodCache* ContextInlineCacheStorage::allocateLoadTypeMethodCache(
     [[maybe_unused]] BCOffset bytecode_offset) {
   return allocateLoadTypeMethodCache();
+}
+
+BinaryOpCache* ContextInlineCacheStorage::allocateBinaryOpCache(
+    [[maybe_unused]] BCOffset bytecode_offset,
+    hir::BinaryOpKind op) {
+  return binary_op_caches_.allocate(op);
 }
 
 StoreAttrCache* ContextInlineCacheStorage::allocateStoreAttrCache(
--- cinderx/Jit/inline_cache_storage.h
+++ cinderx/Jit/inline_cache_storage.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include "cinderx/Common/bump_arena.h"
 #include "cinderx/Common/slab_arena.h"
 #include "cinderx/Jit/bytecode_offsets.h"
 #include "cinderx/Jit/inline_cache.h"
@@ -11,6 +12,73 @@
 namespace cinderx::jit {
 
 using InlineCacheStats = std::vector<CacheStats>;
+
+struct InlineCacheSite {
+  enum class Kind {
+    kLoadAttr,
+    kLoadMethod,
+    kLoadModuleAttr,
+    kLoadModuleMethod,
+    kLoadTypeAttr,
+    kLoadTypeMethod,
+    kBinaryOp,
+    kStoreAttr,
+  };
+
+  InlineCacheSite(BCOffset bytecode_offset, LoadAttrCache* cache);
+  InlineCacheSite(BCOffset bytecode_offset, LoadMethodCache* cache);
+  InlineCacheSite(BCOffset bytecode_offset, LoadModuleAttrCache* cache);
+  InlineCacheSite(BCOffset bytecode_offset, LoadModuleMethodCache* cache);
+  InlineCacheSite(BCOffset bytecode_offset, LoadTypeAttrCache* cache);
+  InlineCacheSite(BCOffset bytecode_offset, LoadTypeMethodCache* cache);
+  InlineCacheSite(BCOffset bytecode_offset, BinaryOpCache* cache);
+  InlineCacheSite(BCOffset bytecode_offset, StoreAttrCache* cache);
+
+  Kind kind;
+  BCOffset bytecode_offset;
+  union {
+    LoadAttrCache* load_attr;
+    LoadMethodCache* load_method;
+    LoadModuleAttrCache* load_module_attr;
+    LoadModuleMethodCache* load_module_method;
+    LoadTypeAttrCache* load_type_attr;
+    LoadTypeMethodCache* load_type_method;
+    BinaryOpCache* binary_op;
+    StoreAttrCache* store_attr;
+  } cache{};
+};
+
+class PerCompilationInlineCacheStorage final {
+ public:
+  const std::vector<InlineCacheSite>& inlineCacheSites() const;
+
+  LoadAttrCache* allocateLoadAttrCache(BCOffset bytecode_offset);
+  LoadMethodCache* allocateLoadMethodCache(BCOffset bytecode_offset);
+  LoadModuleAttrCache* allocateLoadModuleAttrCache(BCOffset bytecode_offset);
+  LoadModuleMethodCache* allocateLoadModuleMethodCache(
+      BCOffset bytecode_offset);
+  LoadTypeAttrCache* allocateLoadTypeAttrCache();
+  LoadTypeAttrCache* allocateLoadTypeAttrCache(BCOffset bytecode_offset);
+  LoadTypeMethodCache* allocateLoadTypeMethodCache();
+  LoadTypeMethodCache* allocateLoadTypeMethodCache(BCOffset bytecode_offset);
+  BinaryOpCache* allocateBinaryOpCache(
+      BCOffset bytecode_offset,
+      hir::BinaryOpKind op);
+  StoreAttrCache* allocateStoreAttrCache(BCOffset bytecode_offset);
+
+  void addLoadTypeAttrCacheSite(
+      BCOffset bytecode_offset,
+      LoadTypeAttrCache* cache);
+  void addLoadTypeMethodCacheSite(
+      BCOffset bytecode_offset,
+      LoadTypeMethodCache* cache);
+
+ private:
+  void addInlineCacheSite(InlineCacheSite site);
+
+  BumpArena inline_cache_arena_;
+  std::vector<InlineCacheSite> inline_cache_sites_;
+};
 
 class ContextInlineCacheStorage final {
  public:
@@ -23,6 +91,9 @@ class ContextInlineCacheStorage final {
       BCOffset bytecode_offset);
   LoadTypeMethodCache* allocateLoadTypeMethodCache();
   LoadTypeMethodCache* allocateLoadTypeMethodCache(BCOffset bytecode_offset);
+  BinaryOpCache* allocateBinaryOpCache(
+      BCOffset bytecode_offset,
+      hir::BinaryOpKind op);
   StoreAttrCache* allocateStoreAttrCache(BCOffset bytecode_offset);
   void addLoadTypeAttrCacheSite(
       BCOffset bytecode_offset,
@@ -41,6 +112,7 @@ class ContextInlineCacheStorage final {
   SlabArena<LoadModuleAttrCache> load_module_attr_caches_;
   SlabArena<LoadModuleMethodCache> load_module_method_caches_;
   SlabArena<LoadTypeMethodCache> load_type_method_caches_;
+  SlabArena<BinaryOpCache> binary_op_caches_;
   SlabArena<StoreAttrCache, AttributeCacheSizeTrait> store_attr_caches_;
 };
 
